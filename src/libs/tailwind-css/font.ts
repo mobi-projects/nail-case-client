e7f5c1292da1f32e8a6f409b63f7c0eb22a92247
keeprok <140785214+keeprok@users.noreply.github.com>
2024-05-30 16:50:45 +0900

    🎉  font , color토큰 추가

---
+++ src/libs/tailwind-css/font.ts
@@ -0,0 +1,62 @@
+export const FONT_STYLES = {
+	largeTitle: {
+		weight: 400,
+		size: "36px",
+		leading: "40px",
+	},
+	title01: {
+		weight: 600,
+		size: "28px",
+		leading: "38px",
+	},
+	title02: {
+		weight: 400,
+		size: "24px",
+		leading: "34px",
+	},
+	title03: {
+		weight: 400,
+		size: "22px",
+		leading: "30px",
+	},
+	headline01: {
+		weight: 800,
+		size: "20px",
+		leading: "30px",
+	},
+	headline02: {
+		weight: 600,
+		size: "18px",
+		leading: "28px",
+	},
+	button: {
+		weight: 600,
+		size: "18px",
+		leading: "24px",
+	},
+	body01: {
+		weight: 400,
+		size: "18px",
+		leading: "24px",
+	},
+	body02: {
+		weight: 400,
+		size: "16px",
+		leading: "22px",
+	},
+	callout: {
+		weight: 400,
+		size: "14px",
+		leading: "20px",
+	},
+	caption01: {
+		weight: 400,
+		size: "12px",
+		leading: "18px",
+	},
+	caption02: {
+		weight: 300,
+		size: "12px",
+		leading: "16px",
+	},
+}
---
+++ src/libs/tailwind-css/styles.ts
@@ -0,0 +1,35 @@
+export const COLORS = {
+	COMMON: {
+		black: "#000000",
+		gray100: "#161616",
+		gray90: "#262626",
+		gray80: "#2E2E2E",
+		gray70: "#525252",
+		gray60: "#6F6F6F",
+		gray50: "#8D8D8D",
+		gray40: "#A8A8A8",
+		gray30: "#C6C6C6",
+		gray20: "#E0E0E0",
+		gray10: "#F4F4F4",
+		white: "#FFFFFF",
+	},
+	Blue: {
+		pb100: "#02ADF2",
+		pb90: "#1BB5F3",
+		pb80: "#35BDF5",
+		pb70: "#4EC6F6",
+		pb60: "#67CEF7",
+		pb50: "#80D6F8",
+	},
+	Yellow: {
+		py100: "#F1F953",
+		py90: "#F2FA64",
+		py80: "#F4FA75",
+		py70: "#F5FB87",
+		py60: "#F7FB98",
+		py50: "#F8FCA9",
+	},
+	BGblue: {
+		BGblue01: "#F6FAFC",
+	},
+}
