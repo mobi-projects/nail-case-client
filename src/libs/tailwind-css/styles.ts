1b7c2dd181a4e60972f9ee5e783da0e211d07f8f
keeprok <140785214+keeprok@users.noreply.github.com>
2024-05-30 21:24:46 +0900

    🔥 : 컨벤션에 맞지않고 상황에 맞지않아 삭제
    * issue: [NAILCASE-38](https://nailcase.atlassian.net/browse/NAILCASE-38)
    desc:
     - 변수의 통일 성필요

--- src/libs/tailwind-css/styles.ts
+++ src/libs/tailwind-css/styles.ts
@@ -1,60 +1,55 @@
 export const COLORS = {
-	COMMON: {
-		black: "#000000",
-		gray100: "#161616",
-		gray90: "#262626",
-		gray80: "#2E2E2E",
-		gray70: "#525252",
-		gray60: "#6F6F6F",
-		gray50: "#8D8D8D",
-		gray40: "#A8A8A8",
-		gray30: "#C6C6C6",
-		gray20: "#E0E0E0",
-		gray10: "#F4F4F4",
-		white: "#FFFFFF",
-	},
-	Blue: {
-		pb100: "#02ADF2",
-		pb90: "#1BB5F3",
-		pb80: "#35BDF5",
-		pb70: "#4EC6F6",
-		pb60: "#67CEF7",
-		pb50: "#80D6F8",
-	},
-	Yellow: {
-		py100: "#F1F953",
-		py90: "#F2FA64",
-		py80: "#F4FA75",
-		py70: "#F5FB87",
-		py60: "#F7FB98",
-		py50: "#F8FCA9",
-	},
-	BGblue: {
-		BGblue01: "#F6FAFC",
-	},
+	black: "#000000",
+	gray100: "#161616",
+	gray90: "#262626",
+	gray80: "#2E2E2E",
+	gray70: "#525252",
+	gray60: "#6F6F6F",
+	gray50: "#8D8D8D",
+	gray40: "#A8A8A8",
+	gray30: "#C6C6C6",
+	gray20: "#E0E0E0",
+	gray10: "#F4F4F4",
+	white: "#FFFFFF",
+
+	pb100: "#02ADF2",
+	pb90: "#1BB5F3",
+	pb80: "#35BDF5",
+	pb70: "#4EC6F6",
+	pb60: "#67CEF7",
+	pb50: "#80D6F8",
+
+	py100: "#F1F953",
+	py90: "#F2FA64",
+	py80: "#F4FA75",
+	py70: "#F5FB87",
+	py60: "#F7FB98",
+	py50: "#F8FCA9",
+
+	BGblue01: "#F6FAFC",
 }
 export const FONT_STYLES = {
-	largeTitle: {
+	LargeTitle: {
 		weight: 400,
 		size: "36px",
 		leading: "40px",
 	},
-	title01: {
+	Title01: {
 		weight: 600,
 		size: "28px",
 		leading: "38px",
 	},
-	title02: {
+	itle02: {
 		weight: 400,
 		size: "24px",
 		leading: "34px",
 	},
-	title03: {
+	Title03: {
 		weight: 400,
 		size: "22px",
 		leading: "30px",
 	},
-	headline01: {
+	Headline01: {
 		weight: 800,
 		size: "20px",
 		leading: "30px",
