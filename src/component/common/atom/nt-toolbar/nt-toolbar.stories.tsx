22e2d2fabf7f7ba57465a93c9334d81c2df3132f
keeprok <140785214+keeprok@users.noreply.github.com>
2024-06-07 21:27:46 +0900

    🎨  NTToolbar 스토리 작성
    * issue:[NAILCASE-55](https://nailcase.atlassian.net/browse/NAILCASE-55)

---
+++ src/component/common/atom/nt-toolbar/nt-toolbar.stories.tsx
@@ -0,0 +1,64 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import NTToolbar from "."
+
+const meta: Meta<typeof NTToolbar> = {
+	title: "component/common/atom/nt-toolbar",
+	parameters: {
+		layout: "centered",
+	},
+	component: NTToolbar,
+	argTypes: {
+		position: {
+			control: false,
+		},
+		topStyle: {
+			control: false,
+		},
+		bottomTextSize: {
+			control: false,
+		},
+		active: {
+			control: "boolean",
+		},
+		onClick: { action: "clicked" },
+	},
+}
+
+export default meta
+
+type Story = StoryObj<typeof NTToolbar>
+
+export const TopDefault: Story = {
+	args: {
+		position: "top",
+		topStyle: "default",
+		active: true,
+		children: 3,
+	},
+}
+
+export const TopLight: Story = {
+	args: {
+		position: "top",
+		topStyle: "light",
+		active: true,
+	},
+}
+
+export const BottomSmall: Story = {
+	args: {
+		position: "bottom",
+		bottomTextSize: "small",
+		active: false,
+	},
+}
+
+export const BottomLarge: Story = {
+	args: {
+		position: "top",
+		bottomTextSize: "small",
+		active: true,
+		topStyle: "default",
+	},
+}
