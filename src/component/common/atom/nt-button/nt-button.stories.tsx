6a3264007550fbdc24ff5955022a3a8634c89edf
HeesikK <7jjhkhz@naver.com>
2024-06-07 23:11:29 +0900

    🎨 NTButton 스토리 작성
    * issue: [NAILCASE-42](https://nailcase.atlassian.net/browse/NAILCASE-42)

---
+++ src/component/common/atom/nt-button/nt-button.stories.tsx
@@ -0,0 +1,64 @@
+import type { Meta, StoryObj } from "@storybook/react"
+
+import { ICON_DATA } from "../../nt-icon"
+
+import { NTButton } from "."
+
+const meta: Meta<typeof NTButton> = {
+	title: "component/common/nt-button",
+	parameters: {
+		layout: "centered",
+	},
+	component: NTButton,
+	argTypes: {
+		variant: {
+			control: "inline-radio",
+			options: ["primary", "secondary", "tertiary"],
+		},
+		size: {
+			control: "inline-radio",
+			options: ["large", "medium", "small"],
+		},
+		disabled: {
+			control: "boolean",
+		},
+		children: {
+			control: "text",
+		},
+		icon: {
+			control: "inline-radio",
+			options: Object.keys(ICON_DATA),
+		},
+	},
+}
+
+export default meta
+
+type Story = StoryObj<typeof NTButton>
+
+export const Primary: Story = {
+	args: {
+		variant: "primary",
+		size: "large",
+		children: "Button",
+		icon: "check",
+	},
+}
+
+export const Secondary: Story = {
+	args: {
+		variant: "secondary",
+		size: "medium",
+		children: "Button",
+		icon: "check",
+	},
+}
+
+export const Tertiary: Story = {
+	args: {
+		variant: "tertiary",
+		size: "small",
+		children: "Button",
+		icon: "check",
+	},
+}
