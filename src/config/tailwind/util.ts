ae94ef38c2c428b628725115e5a616300653b3c4
HeesikK <7jjhkhz@naver.com>
2024-06-08 00:44:59 +0900

    ⚡️ cn 함수 수정
    * issue: [NAILCASE-42](https://nailcase.atlassian.net/browse/NAILCASE-42)

--- src/config/tailwind/util.ts
+++ src/config/tailwind/util.ts
@@ -1,4 +1,26 @@
 import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs))
+import { extendTailwindMerge } from "tailwind-merge"
+const customTwMerge = extendTailwindMerge({
+	extend: {
+		classGroups: {
+			"font-size": [
+				"text-LargeTitle",
+				"text-Title01",
+				"text-Title02",
+				"text-Title03",
+				"text-Headline01",
+				"text-Headline02",
+				"text-Button",
+				"text-Body01",
+				"text-Body02",
+				"text-Callout",
+				"text-Caption01",
+				"text-Caption02",
+			],
+			animate: ["animate-in", "animate-out", "animate-none"],
+		},
+	},
+})
+export function cn(...args: ClassValue[]) {
+	return customTwMerge(clsx(...args))
+}
