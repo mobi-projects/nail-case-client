cbedfcb7d3dddbff255d7e500401898a19e724f4
keeprok <140785214+keeprok@users.noreply.github.com>
2024-06-09 07:57:43 +0900

    🎨 NTNameBox 컴포넌트 생성
    * issue:[NAILCASE-65]-(https://nailcase.atlassian.net/browse/NAILCASE-65)

---
+++ src/component/common/nt-name-box/index.tsx
@@ -0,0 +1,21 @@
+import type { VariantProps } from "class-variance-authority"
+import { cva } from "class-variance-authority"
+
+const NameBoxVarants = cva(
+	"w-[63px] bg- h-[28px] rounded-[8px]  text-Callout text-Gray100 flex items-center justify-center font-SemiBold",
+	{
+		variants: {
+			bgColor: {
+				blue: "bg-BGblue02",
+				yellow: "bg-PY50",
+				gray: "bg-Gray20",
+			},
+		},
+	},
+)
+type NTNameBoxPT = VariantProps<typeof NameBoxVarants> & {
+	children: React.ReactNode
+}
+export default function NTNameBox({ bgColor, children }: NTNameBoxPT) {
+	return <div className={NameBoxVarants({ bgColor })}>{children}</div>
+}
