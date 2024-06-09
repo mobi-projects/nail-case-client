7b48970d58b79d72b4628552e5755f0c5caf63d3
keeprok <140785214+keeprok@users.noreply.github.com>
2024-06-09 09:53:03 +0900

    🔥 css 제거
     * issue:[NAILCASE-55](https://nailcase.atlassian.net/browse/NAILCASE-55)

--- src/component/common/atom/nt-toolbar/index.tsx
+++ src/component/common/atom/nt-toolbar/index.tsx
@@ -94,7 +94,7 @@ export default function NTToolbar({
 	isSelected,
 }: NTToolbarPT) {
 	return (
-		<div className="flex h-[full] w-[full] gap-x-[20px]">
+		<div className="flex h-full w-full gap-x-[20px]">
 			{arr.map((children, idx) => (
 				<NTToolbarSingle
 					key={idx}
