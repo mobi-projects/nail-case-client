0b18fb12de53608564bce657ee41799cf808b835
yunshin <2younsin@gmail.com>
2024-06-10 21:17:59 +0900

    🤡 예약 목록 관련, mock data 생성 함수 작성
    * issue: [NAILCASE-72](https://nailcase.atlassian.net/browse/NAILCASE-72)
    
    desc:
     - mock 생성 함수 중, 비슷한 패턴의 코드 분리
     - 다른 파일에서 호출하는 함수에 대해서만 export

--- src/mook/create.ts
+++ src/mook/create.ts
@@ -1,63 +1,22 @@
 import { faker } from "@faker-js/faker/locale/ko"
 
 import type {
+	TArtist,
 	TNTTime,
 	TOperating,
 	TSchedule,
 	TShopGuide,
 	TShopInfo,
-	TSpecialty,
 	TUser,
 } from "@/type"
+import type {
+	TEssentialForm,
+	TNailCondition,
+	TReservation,
+	TReservationStatus,
+} from "@/type/reservation"
 
-export const createNTTime = (): TNTTime => {
-	const current = new Date()
-	const ntTime: TNTTime = {
-		year: current.getFullYear(),
-		month: current.getMonth() + 1,
-		day: current.getDate(),
-		hour: current.getHours(),
-		minute: current.getMinutes(),
-		division: current.getHours() > 12 ? "pm" : "am",
-	}
-	return ntTime
-}
-export const createOperating = (): TOperating => {
-	const operating = {
-		dayOfWeek: (Math.random() * 10) % 7,
-		startTime: createNTTime(),
-		endTime: createNTTime(),
-	}
-	return operating
-}
-export const createSchedule = (): TSchedule => {
-	const schedule: TSchedule = {
-		id: faker.string.uuid(),
-		startTime: createNTTime(),
-		endTime: createNTTime(),
-	}
-	return schedule
-}
-export const createOwner = (): TUser => {
-	const owner: TUser = {
-		id: faker.string.uuid(),
-		name: faker.internet.userName(),
-		email: faker.internet.email(),
-		phone: faker.phone.number(),
-		scheduleArr: Array.from({ length: 2 }, () => createSchedule()),
-	}
-	return owner
-}
-export const createGuide = (): TShopGuide => {
-	const guide: TShopGuide = {
-		parking: Number(Math.random() * 10),
-		companion: Number(Math.random() * 10),
-		reservationDeadline: Number(Math.random() * 10),
-	}
-	return guide
-}
 export const createShopInfo = (): TShopInfo => {
-	const specialties = ["eyebrow", "padi", "nail"]
 	const shopInfo: TShopInfo = {
 		id: faker.string.uuid(),
 		address: faker.location.streetAddress(),
@@ -69,10 +28,127 @@ export const createShopInfo = (): TShopInfo => {
 		hashtagArr: Array.from({ length: 3 }, () => "#" + faker.lorem.word()),
 		snsArr: Array.from({ length: 4 }, () => faker.internet.url()),
 		srcArr: Array.from({ length: 5 }, () => faker.image.url()),
-		owner: createOwner(),
-		specialty: specialties[(Math.random() * 10) % 3] as TSpecialty,
+		owner: createUser(),
+		specialty: pickRandomOneOfArr(["NAIL", "PADI", "EYEBROW"]),
 		guide: createGuide(),
-		operatingTimeArr: Array.from({ length: 7 }, () => createOperating()),
+		operatingTimeArr: getFakeObjArr(7, createOperating),
 	}
 	return shopInfo
+}
+export const createReservationArr = (): TReservation[] =>
+	getFakeObjArr(getRandomNumber(10), createReservation)
+
+const createNTTime = (): TNTTime => {
+	const current = new Date()
+	const ntTime: TNTTime = {
+		year: current.getFullYear(),
+		month: current.getMonth() + 1,
+		day: current.getDate(),
+		hour: current.getHours(),
+		minute: current.getMinutes(),
+		division: current.getHours() > 12 ? "PM" : "AM",
+	}
+	return ntTime
+}
+const createOperating = (): TOperating => {
+	const operating = {
+		dayOfWeek: (Math.random() * 10) % 7,
+		startTime: createNTTime(),
+		endTime: createNTTime(),
+	}
+	return operating
+}
+const createSchedule = (): TSchedule => {
+	const schedule: TSchedule = {
+		id: faker.number.int(),
+		reservationId: faker.number.int(),
+		startTime: createNTTime(),
+		endTime: createNTTime(),
+	}
+	return schedule
+}
+const createUser = (): TUser => {
+	const user: TUser = {
+		id: faker.number.int(),
+		name: faker.internet.userName(),
+		email: faker.internet.email(),
+		phone: faker.phone.number(),
+	}
+	return user
+}
+const createGuide = (): TShopGuide => {
+	const guide: TShopGuide = {
+		parking: Number(getRandomNumber(3)),
+		companion: Number(getRandomNumber(3)),
+		reservationDeadline: Number(getRandomNumber(3)),
+	}
+	return guide
+}
+const createArtist = (): TArtist => {
+	const user = createUser()
+	const artist: TArtist = {
+		...user,
+		specialty: "NAIL",
+		workingTimeArr: getFakeObjArr(7, createOperating),
+		scheduleArr: getFakeObjArr(getRandomNumber(2), createSchedule),
+	}
+	return artist
+}
+const createNailCondition = () => {
+	const allNailConditions: TNailCondition[] = [
+		"REPAIR",
+		"AS",
+		"WOUND_CARE",
+		"CORRECTION",
+	]
+	const randomIdx = getRandomNumber(allNailConditions.length - 1)
+	return allNailConditions[randomIdx]
+}
+const createNailConditionArr = (): TNailCondition[] => {
+	const conditionArr = getFakeObjArr(getRandomNumber(4), createNailCondition)
+	return [...new Set(conditionArr)]
+}
+const createEssentialForm = () => {
+	const essentialForm: TEssentialForm = {
+		reservationDate: createNTTime(),
+		treatment: pickRandomOneOfArr(["AOM", "CARE", "ONE", "PICTURE"]),
+		removalReq: pickRandomOneOfArr(["IN-SHOP", "ELSE-WHERE", "NO-NEED"]),
+		extensionReq: getRandomBoolean(),
+		conditionArr: createNailConditionArr(),
+	}
+	return essentialForm
+}
+const createStatus = (): TReservationStatus => {
+	const statusArr: TReservationStatus[] = [
+		"WAITING",
+		"APPROVAL",
+		"CANCELED",
+		"REJECTED",
+	]
+	const randomIdx = getRandomNumber(statusArr.length - 1)
+	return statusArr[randomIdx]
+}
+const createReservation = (): TReservation => {
+	const companion = getRandomNumber(2) // 동반자 수
+	const totConsumer = companion + 1 // 방문 고객수 = 동반자 수 + 예약자 본인
+	const reservation: TReservation = {
+		id: faker.number.int(),
+		status: createStatus(),
+		artistArr: getFakeObjArr(totConsumer, createArtist),
+		customer: createUser(),
+		schedule: createSchedule(),
+		companion: getRandomNumber(3),
+		essentialDetailArr: getFakeObjArr(totConsumer, createEssentialForm),
+		customDetailArr: [],
+	}
+	return reservation
+}
+const getFakeObjArr = <T>(arrayLength: number, createFunc: () => T) =>
+	Array.from({ length: arrayLength }, () => createFunc())
+const getRandomNumber = (upperLimit: number = 1) =>
+	Math.floor((Math.random() * 10) % (upperLimit + 1))
+const getRandomBoolean = (): boolean => [true, false][getRandomNumber(1)]
+const pickRandomOneOfArr = <T>(arr: T[]): T => {
+	const randomIdx = getRandomNumber(arr.length - 1)
+	return arr[randomIdx]
 }
