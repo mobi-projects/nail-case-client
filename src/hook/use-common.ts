c74a92cb56757a0ebdac2e4441eb067dd16b929f
yunshin <2younsin@gmail.com>
2024-06-10 21:21:38 +0900

    ✨ 예약목록 조회 hook 작성
    * issue: [NAILCASE-72](https://nailcase.atlassian.net/browse/NAILCASE-72)
    
    desc:
     - 관련 query-key 상수화 (src > constant > tanstack-key)
     - 패칭함수 작성 (src > util > api > get)
     - useQuery 활용 (src > hook > use-common)

--- src/hook/use-common.ts
+++ src/hook/use-common.ts
@@ -1,7 +1,7 @@
 import { useQuery } from "@tanstack/react-query"
 
-import { QUERY_SHOP_INFO } from "@/constant"
-import { getShopInfo } from "@/util/api"
+import { QUERY_RESERVATION_ARR, QUERY_SHOP_INFO } from "@/constant"
+import { getReservationArr, getShopInfo } from "@/util/api"
 
 export const useShopInfo = () => {
 	const { data: shopInfo } = useQuery({
@@ -9,4 +9,12 @@ export const useShopInfo = () => {
 		queryFn: getShopInfo,
 	})
 	return { shopInfo }
+}
+
+export const useReservationArr = () => {
+	const { data: reservationArr } = useQuery({
+		queryKey: [QUERY_RESERVATION_ARR],
+		queryFn: getReservationArr,
+	})
+	return { reservationArr }
 }
--- src/util/api/get.ts
+++ src/util/api/get.ts
@@ -1,7 +1,13 @@
-import { createShopInfo, tmpFetch } from "@/mook"
+import { createReservationArr, createShopInfo, tmpFetch } from "@/mook"
 import type { TShopInfo } from "@/type"
+import type { TReservation } from "@/type/reservation"
 
 export const getShopInfo = async () => {
-	const response = await tmpFetch<TShopInfo>(createShopInfo())
+	const response = await tmpFetch<TShopInfo>(createShopInfo()) // TODO: fetching 함수 교체 필요
+	return response
+}
+export const getReservationArr = async () => {
+	const response = await tmpFetch<TReservation[]>(createReservationArr()) // TODO: fetching 함수 교체 필요
+	console.log(response)
 	return response
 }
