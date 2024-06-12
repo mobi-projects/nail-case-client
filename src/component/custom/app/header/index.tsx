861604968706750e4618df3e459296354e982657
keeprok <140785214+keeprok@users.noreply.github.com>
2024-06-12 11:20:54 +0900

    🔥 pulldown 삭제
    * issue: [NAILCASE](https://nailcase.atlassian.net/browse/NAILCASE-90)

--- src/component/custom/app/header/index.tsx
+++ src/component/custom/app/header/index.tsx
@@ -2,35 +2,23 @@
 //지워질예정
 import { useRef } from "react"
 
-import NTPulldown from "@/component/common/atom/nt-pulldown"
 import NTIcon from "@/component/common/nt-icon"
+import NTLogo from "@/component/common/nt-logo"
 import NTSearchfield from "@/component/common/nt-searchfield"
-import { usePulldown } from "@/hook/use-component"
 
 export default function Header() {
-	const { isOpen, onClickItems, onClickTrigger, onClickWrapper, optionArr } =
-		usePulldown(["발행한 소식", "임시저장", "예약"])
 	const searchInputRef = useRef<HTMLInputElement>(null)
 
 	return (
 		<div className="flex flex-col items-center justify-center pt-[68px]">
 			<div className="flex w-full max-w-[1200px] flex-col">
-				<div className="h-[38px] w-[134px] text-3xl text-Gray90">newtips</div>
-				{/* 변경예정 */}
+				<NTLogo />
 				<div className="flex w-full justify-between gap-[70px] pb-[18px]">
 					<div>
-						<NTPulldown
-							{...{
-								isOpen,
-								onClickItems,
-								onClickTrigger,
-								onClickWrapper,
-								optionArr,
-							}}
-							//지워질예정
-						></NTPulldown>
+						<div
+						//NTPullDown pr 수정시 지워질예정
+						></div>
 					</div>
-
 					<div className="flex h-fit w-full justify-between">
 						<div>
 							<NTSearchfield size="large" ref={searchInputRef} />
