e0e92a05930db7cf388182d9d857034864fe6ffb
keeprok <140785214+keeprok@users.noreply.github.com>
2024-06-13 18:54:44 +0900

    ⚡️ 이미지 최적화
    * issue: [NAILCASE](https://nailcase.atlassian.net/browse/NAILCASE-90)
    desc:

--- src/component/common/nt-logo/index.tsx
+++ src/component/common/nt-logo/index.tsx
@@ -1,9 +1,8 @@
-// src/component/common/Logo.tsx
 import Image from "next/image"
 import React from "react"
 
 import logoSrc from "../../../../public/asset/nt-logo.svg"
 
 export default function NTLogo() {
-	return <Image src={logoSrc} alt="Logo" />
+	return <Image src={logoSrc} alt="Logo" width={134} height={38} />
 }
