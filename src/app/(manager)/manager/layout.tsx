aefe7105f210086e11a9b6793d2755f49a2848b3
yunshin <2younsin@gmail.com>
2024-06-16 20:47:44 +0900

    🎨 매장 운영자 페이지 custom component 인덱싱

--- src/app/(manager)/manager/layout.tsx
+++ src/app/(manager)/manager/layout.tsx
@@ -1,15 +1,9 @@
 "use client"
-import Header from "@/component/custom/home/header"
 
 export default function ManagerLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode
 }>) {
-	return (
-		<div className="max-w-dvw">
-			<Header />
-			{children}
-		</div>
-	)
+	return <div className="max-w-dvw">{children}</div>
 }
---
+++ src/component/custom/manager/my-shop/home/01/index.tsx
@@ -0,0 +1,3 @@
+export default function Manager_MyShop_Home_01() {
+	return <div className="h-[432.47px] w-full border-[5px] border-orange-300" />
+}
