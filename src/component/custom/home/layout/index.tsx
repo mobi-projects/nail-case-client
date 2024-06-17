80596194685d9ef0bd338e518197c02ff630d13e
snkchan <144839872+snkchan@users.noreply.github.com>
2024-06-17 18:07:33 +0900

    🎨 home page layout 작업
    * issue: [NAILCASE-113](https://nailcase.atlassian.net/browse/NAILCASE-113)
    sub-issues:
    - [NAILCASE-117](https://nailcase.atlassian.net/browse/NAILCASE-117)

--- src/component/custom/home/layout/index.tsx
+++ src/component/custom/home/layout/index.tsx
@@ -1,6 +1,7 @@
 "use client"
 import BannerCarousel from "@/component/common/nt-banner-carousel"
 import NTContent from "@/component/common/nt-content"
+import NTIcon from "@/component/common/nt-icon"
 import { useShopInfo } from "@/hook/use-common"
 import { useBanner } from "@/hook/use-component"
 import type { TShopInfo } from "@/type"
@@ -12,32 +13,45 @@ type BannerItemPT = {
 export default function ShopShowCase() {
 	const { handleCarousel, carouselIdx } = useBanner()
 	const { shopInfo } = useShopInfo()
-	if (!shopInfo) return <h1>Loading Banner....</h1>
+	if (!shopInfo)
+		return <div className="h-[30rem] w-full">Loading Banner....</div>
 	return (
 		<div className="h-[30rem] w-full">
 			<div className="absolute left-0 top-0 h-full w-full">
 				<BannerCarousel type="user" handleCarousel={handleCarousel}>
+					<NTIcon
+						icon="back"
+						className="absolute left-[310px] top-20 h-6 w-9 text-White"
+					/>
+					<NTIcon
+						icon="home"
+						className="absolute left-[345px] top-20 h-6 w-6 text-White"
+					/>
+
+					<NTIcon
+						icon="share"
+						className="absolute right-[360px] top-20 h-7 w-7 text-White"
+					/>
 					<BannerHeader shopInfo={shopInfo} />
-					<BannerDesciption shopInfo={shopInfo} />
 					<NTContent
 						mode="dark"
-						className="absolute right-80 top-24"
+						className="absolute right-[360px] top-40"
 					>{`${carouselIdx + 1}/${shopInfo.srcArr.length}`}</NTContent>
 				</BannerCarousel>
+				<BannerDesciption shopInfo={shopInfo} />
 			</div>
 		</div>
 	)
 }
 
 function BannerHeader({ shopInfo }: BannerItemPT) {
-	const { specialty, address, shopName, todayAccess, totalAccess } = shopInfo
+	const { specialty, address, shopName } = shopInfo
 	return (
-		<div className="absolute left-60 top-24 z-10">
+		<div className="absolute left-[310px] top-40">
 			<p className="text-Callout text-[14px] font-Light text-White">{`${specialty}  |  ${address}`}</p>
 			<h1 className="text-Title01 text-[28px] font-Bold text-White">
 				{shopName}
 			</h1>
-			<p className="text-Callout font-SemiBold text-Gray20">{`오늘 ${todayAccess.toLocaleString("ko-KR")} · 전체 ${totalAccess.toLocaleString("ko-KR")}`}</p>
 		</div>
 	)
 }
@@ -45,7 +59,7 @@ function BannerHeader({ shopInfo }: BannerItemPT) {
 function BannerDesciption({ shopInfo }: BannerItemPT) {
 	const { hashtagArr, overview } = shopInfo
 	return (
-		<div className="absolute left-60 top-72 z-10">
+		<div className="absolute left-[310px] top-80 z-10">
 			<div className="flex gap-3">
 				{hashtagArr.map((hashtag, idx) => (
 					<p
