34b8940886108d253ad708dafd1448842b27d975
yunshin <2younsin@gmail.com>
2024-06-17 17:28:45 +0900

    🎨 manager/sub/layout CCI
    * issue: [NAILCASE-112](https://nailcase.atlassian.net/browse/NAILCASE-112)
    sub-issues:
     - [NAILCASE-119](https://nailcase.atlassian.net/browse/NAILCASE-119)

---
+++ src/component/custom/manager/sub/layout/01/index.tsx
@@ -0,0 +1,7 @@
+export function Manager_Sub_Layout_01() {
+	return (
+		<div className="h-[131px] w-full bg-transparent">
+			<div className="absolute left-0 top-0 h-[131px] w-full border-[5px] border-red-500" />
+		</div>
+	)
+}
