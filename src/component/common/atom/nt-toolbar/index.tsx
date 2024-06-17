82bfa6bb67354caf0caff99aaef38c5548d2a088
keeprok <140785214+keeprok@users.noreply.github.com>
2024-06-17 21:11:11 +0900

    🎨 toolbar 폰트크기 조정
    *issue : [NAILCASE-118](https://nailcase.atlassian.net/browse/NAILCASE-118)
    sub-issues:
    - [NAILCASE-55](https://nailcase.atlassian.net/browse/NAILCASE-55)

--- src/component/common/atom/nt-toolbar/index.tsx
+++ src/component/common/atom/nt-toolbar/index.tsx
@@ -10,49 +10,52 @@ type NTToolbarSinglePT = {
 	isSelected: VoidFunction
 }
 
-const TollbarVariants = cva("flex justify-center m-0 p-0 cursor-pointer", {
-	variants: {
-		position: {
-			top: "items-end  w-[81.5px] h-[38px] hover:text-Gray50 text-Body01  text-Black",
-			bottom: "items-start w-[110px] h-[44px] hover:text-PB50 text-Gray50 ",
+const TollbarVariants = cva(
+	"flex justify-center m-0 p-0 cursor-pointer font-SemiBold",
+	{
+		variants: {
+			position: {
+				top: "items-end  w-[81.5px] h-[38px] hover:text-Gray50 text-Body01  text-Black",
+				bottom: "items-start w-[110px] h-[44px] hover:text-PB50 text-Gray50 ",
+			},
+			topStyle: {
+				default: "border-PB100",
+				light: "border-PB80",
+			},
+			bottomTextSize: {
+				small: "text-Body01",
+				large: "text-Title03",
+			},
+			select: {
+				true: "pointer-events-none",
+				false: "",
+			},
 		},
-		topStyle: {
-			default: "border-PB100",
-			light: "border-PB80",
-		},
-		bottomTextSize: {
-			small: "text-Body01",
-			large: "text-Title03",
-		},
-		select: {
-			true: "pointer-events-none",
-			false: "",
-		},
+		compoundVariants: [
+			{ position: "top", topStyle: "default" },
+			{ position: "top", topStyle: "light" },
+			{ position: "bottom", bottomTextSize: "small" },
+			{ position: "bottom", bottomTextSize: "large" },
+			{
+				position: "top",
+				topStyle: "default",
+				select: true,
+				className: "text-PB100 border-t-2",
+			},
+			{
+				position: "top",
+				topStyle: "light",
+				select: true,
+				className: "text-PB80 border-t-2",
+			},
+			{
+				position: "bottom",
+				select: true,
+				className: "border-b-2 text-PB100 border-PB100",
+			},
+		],
 	},
-	compoundVariants: [
-		{ position: "top", topStyle: "default" },
-		{ position: "top", topStyle: "light" },
-		{ position: "bottom", bottomTextSize: "small" },
-		{ position: "bottom", bottomTextSize: "large" },
-		{
-			position: "top",
-			topStyle: "default",
-			select: true,
-			className: "text-PB100 border-t-2",
-		},
-		{
-			position: "top",
-			topStyle: "light",
-			select: true,
-			className: "text-PB80 border-t-2",
-		},
-		{
-			position: "bottom",
-			select: true,
-			className: "border-b-2 text-PB100 border-PB100",
-		},
-	],
-})
+)
 
 function NTToolbarSingle({
 	children,
