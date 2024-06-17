34f3b276423a677fdf334a29914c3018aedd5ecb
keeprok <140785214+keeprok@users.noreply.github.com>
2024-06-17 21:26:04 +0900

    🎨 manager home 04 수정
    *issue : [NAILCASE-118](https://nailcase.atlassian.net/browse/NAILCASE-118)
    sub-issues:
    - [NAILCASE-12](https://nailcase.atlassian.net/browse/NAILCASE-124)
    - [NAILCASE-125](https://nailcase.atlassian.net/browse/NAILCASE-125)
    desc:
    - manager home 03 배치

--- src/app/(manager)/manager/base/(home)/page.tsx
+++ src/app/(manager)/manager/base/(home)/page.tsx
@@ -1,20 +1,33 @@
 "use client"
 
+import { useState } from "react"
+
+import { NTButton } from "@/component/common/atom/nt-button"
 import BannerCarousel from "@/component/common/nt-banner-carousel"
 import NTContent from "@/component/common/nt-content"
+import ReservationForm from "@/component/custom/home-admin/reservation-form"
 import { NoticeCard } from "@/component/custom/post/notice-card"
+import { PostCard } from "@/component/custom/post/post-card"
 import ReservationConfirmedCard from "@/component/custom/reservation/reservation-confirmed"
 import ReservationPendingCard from "@/component/custom/reservation/reservation-pending"
 import { useShopInfo } from "@/hook/use-common"
 import { useBanner } from "@/hook/use-component"
+import { createPostArr } from "@/mook"
 import type { TShopInfo } from "@/type"
+import type { TPost } from "@/type/post"
 
 export default function Home() {
+	const cardListTilte = ["전체", "인기글", "공지글"]
+	const [isSelected, setIsSelected] = useState(0)
 	const { carouselIdx, handleCarousel } = useBanner()
 	const { shopInfo } = useShopInfo()
+	const postArr = createPostArr()
 	if (!shopInfo) return <h1>Banner Image..</h1>
+	const hadleSelected = (idx: number) => {
+		setIsSelected(idx)
+	}
 	return (
-		<div className="flex flex-col gap-10 p-3">
+		<div className="flex w-full flex-col gap-10 p-3">
 			<div className="h-[380px] w-full">
 				<BannerCarousel type="manager" handleCarousel={handleCarousel}>
 					<BannerHeader shopInfo={shopInfo} />
@@ -26,14 +39,36 @@ export default function Home() {
 				</BannerCarousel>
 			</div>
 			<div className="flex flex-col gap-[20px]">
-				<p className="block text-Title03">오늘 하루 예약 일정을 살펴볼게요.</p>
+				<p className="block text-Title03 font-SemiBold text-Gray100">
+					오늘 하루 예약 일정을 살펴볼게요.
+				</p>
 				<_TodaysReservationSummary />
-				<_ReservationDetailArr />
+				<ReservationForm />
 			</div>
 
 			<HomeDivider />
-
-			<CardList />
+			<div className="flex flex-col gap-[18px] pb-[100px]">
+				<div>
+					<div className="text-Title03 font-SemiBold text-Gray100">
+						내 샵 소식을 전달해요
+					</div>
+					<div className="flex justify-between">
+						<div className="flex items-center gap-[18px]">
+							{cardListTilte.map((title, idx) => (
+								<div
+									key={idx}
+									onClick={() => hadleSelected(idx)}
+									className={`cursor-pointer text-Title03 font-SemiBold ${isSelected === idx ? "text-PB100" : "text-Gray50"}`}
+								>
+									{title}
+								</div>
+							))}
+						</div>
+						<NTButton size="large">소식 작성하기</NTButton>
+					</div>
+				</div>
+				<CardList postArr={postArr} />
+			</div>
 		</div>
 	)
 }
@@ -83,44 +118,59 @@ function _TodaysReservationSummary() {
 	)
 }
 
-function _ReservationDetailArr() {
-	return (
-		<div className="h-[646.39px] w-full rounded-[26px] bg-red-300 shadow-[#E0E0E0]" />
-	)
-}
+// function _ReservationDetailArr() {
+// 	return (
+// 		<div className="h-[646.39px] w-full rounded-[26px] bg-red-300 shadow-[#E0E0E0]" />
+// 	)
+// }
 
 function HomeDivider() {
 	return (
 		<div className="relative h-[12px] w-full bg-White">
-			<div className="absolute left-0 h-full w-full scale-[200%] border-y-2 border-[#80D6F8]/40 bg-White" />
+			<div className="absolute left-0 h-full w-full scale-[200%] border-y border-[#80D6F8]/40 bg-White" />
 		</div>
 	)
 }
 
-function CardList() {
-	const itemArr = Array.from({ length: 16 })
+type CardListProps = {
+	postArr: TPost[]
+}
+
+const CardList: React.FC<CardListProps> = ({ postArr }) => {
 	return (
 		<div className="flex h-fit w-[1200px] items-center overflow-y-hidden overflow-x-scroll">
-			{itemArr.map((item, idx) => {
+			<NoticeCard
+				id={0}
+				category={"NEWS"}
+				srcArr={[]}
+				title={""}
+				content={""}
+				likes={0}
+				views={0}
+				comments={0}
+				createdAt={{
+					year: 0,
+					month: 0,
+					day: 0,
+					hour: 0,
+					minute: 0,
+					division: "PM",
+				}}
+				commentArr={[]}
+			/>
+			{postArr.map((post, idx) => {
 				return (
-					<NoticeCard
+					<PostCard
 						key={idx}
-						id={0}
+						createdAt={post.createdAt}
+						title={post.title}
+						likes={post.likes}
+						comments={post.comments}
+						id={idx}
 						category={"NEWS"}
 						srcArr={[]}
-						title={""}
 						content={""}
-						likes={0}
 						views={0}
-						comments={0}
-						createdAt={{
-							year: 0,
-							month: 0,
-							day: 0,
-							hour: 0,
-							minute: 0,
-							division: "PM",
-						}}
 						commentArr={[]}
 					/>
 				)
