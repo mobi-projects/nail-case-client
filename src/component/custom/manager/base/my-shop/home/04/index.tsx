2d9c140a38444b3d65bbcccac25bb80fe50fc1f3
HeesikK <7jjhkhz@naver.com>
2024-06-19 11:28:36 +0900

    🎨 manager-base-my-shop-home-04-02 퍼블리싱 완료
    * issue: [NAILCASE-132](https://nailcase.atlassian.net/browse/NAILCASE-132)

--- src/component/custom/manager/base/my-shop/home/04/index.tsx
+++ src/component/custom/manager/base/my-shop/home/04/index.tsx
@@ -1,14 +1,69 @@
+"use client"
+
+import NTOption from "@/component/common/nt-option"
+import Slider from "@/component/common/nt-slider"
+import { useOption } from "@/hook/use-component"
+
 export default function Manager_Base_MyShop_Home_04() {
 	return (
-		<div className="grid h-[586px] w-full grid-cols-[1.5fr_1fr] gap-[15px] border-[5px] border-orange-300">
+		<div className="flex justify-between pb-[30px]">
 			<Manager_Base_MyShop_Home_04_01 />
 			<Manager_Base_MyShop_Home_04_02 />
 		</div>
 	)
 }
 function Manager_Base_MyShop_Home_04_01() {
-	return <div className="h-full w-full border-[5px] border-green-300" />
+	return <div />
 }
 function Manager_Base_MyShop_Home_04_02() {
-	return <div className="h-full w-full border-[5px] border-green-300" />
+	const categories = [
+		{
+			title: "커스텀",
+			options: ["커스텀 필요"],
+		},
+		{
+			title: "예약금",
+			options: ["1만원", "2만원", "3만원", "4만원"],
+		},
+		{
+			title: "회원권 유무",
+			options: ["연장 필요", "연장 필요 없음"],
+		},
+		{
+			title: "컨디션",
+			options: ["영양 시술", "랩핑 필요"],
+		},
+	]
+
+	// eslint-disable-next-line react-hooks/rules-of-hooks
+	const optionsData = categories.map((category) => useOption(category.options))
+
+	return (
+		<div className="flex h-[586px] w-[486px] flex-col justify-center rounded-[26px] border px-[25px] drop-shadow">
+			<div className="mt-[20px] flex justify-end"></div>
+			{categories.map((category, index) => (
+				<div key={index} className="mb-[15px]">
+					{index === 0 && <div className="mt-[10px]"></div>}
+					<div className="mb-[10px] text-Headline02 text-Gray70">
+						{category.title}
+					</div>
+					<NTOption
+						disabled
+						onClickOption={optionsData[index].onClickOption}
+						optionArr={optionsData[index].optionArr}
+						checkedOption={optionsData[index].checkedOption}
+						itemsPerRow={4}
+					/>
+					{index === 0 && (
+						<div className="mt-[20px]">
+							<Slider upperLimit={10} curValue={0} />
+						</div>
+					)}
+					{index < categories.length - 1 && (
+						<div className="mt-[15px] border-t-[1.5px] border-Gray10"></div>
+					)}
+				</div>
+			))}
+		</div>
+	)
 }
