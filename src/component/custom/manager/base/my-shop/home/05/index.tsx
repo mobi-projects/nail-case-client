61a09ab08c31d7e5136e266fa59aaa3f4e8fbb19
HeesikK <7jjhkhz@naver.com>
2024-06-19 10:56:46 +0900

    🔥 불필요한 코드 삭제
    * issue: [NAILCASE-133](https://nailcase.atlassian.net/browse/NAILCASE-133)

--- src/component/custom/manager/base/my-shop/home/05/index.tsx
+++ src/component/custom/manager/base/my-shop/home/05/index.tsx
@@ -8,8 +8,6 @@ import type { TPost } from "@/type/post"
 export default function Manager_Base_MyShop_Home_05() {
 	const { postArr } = usePostArr()
 
-	console.log(postArr)
-
 	return (
 		<div className="flex flex-col">
 			{postArr?.map((post, idx) => {
@@ -27,7 +25,7 @@ function Manager_Base_MyShop_Home_05_01({ post }: postPT) {
 	return (
 		<div className="mb-[20px] flex h-[260px] w-full items-center rounded-[26px] border px-[40px] drop-shadow">
 			<Image
-				src={post?.srcArr[0]}
+				src={post.srcArr[0]}
 				alt="Notice Image"
 				width={346}
 				height={204}
@@ -36,15 +34,15 @@ function Manager_Base_MyShop_Home_05_01({ post }: postPT) {
 			/>
 			<div className="ml-[50px] flex h-[204px] flex-col justify-between">
 				<div className="border-b-Gray10 pb-[15px] text-Title03">
-					{post?.title}
+					{post.title}
 				</div>
 				<div className="border-t-[1.5px] border-Gray10 pb-[15px]"></div>
 				<div className="flex w-[761.77px] justify-between">
 					<div className="line-clamp-5 w-[346px] text-Headline02 text-Gray50">
-						{post?.content}
+						{post.content}
 					</div>
 					<div className="flex items-end text-Callout text-Gray30">
-						좋아요 {post?.likes} · 댓글 {post?.comments}
+						좋아요 {post.likes} · 댓글 {post.comments}
 					</div>
 				</div>
 			</div>
