740d8bca0d79eaec70e804835ffc406232f2bce1
yunshin <2younsin@gmail.com>
2024-06-20 04:39:13 +0900

    🎨 manager/base/my-shop/review CCI
    * issue: [NAILCASE-139](https://nailcase.atlassian.net/browse/NAILCASE-139)
    sub-issues:
     - [NAILCASE-149](https://nailcase.atlassian.net/browse/NAILCASE-149)

---
+++ src/component/custom/manager/base/my-shop/review/02/index.tsx
@@ -0,0 +1,14 @@
+export default function Manager_Base_MyShop_Review_02() {
+	return (
+		<div className="flex h-[50px] w-full items-center justify-between border-[5px] border-orange-300">
+			<Manager_Base_MyShop_Review_02_01 />
+			<Manager_Base_MyShop_Review_02_02 />
+		</div>
+	)
+}
+function Manager_Base_MyShop_Review_02_01() {
+	return <div className="h-full w-full border-[5px] border-green-300" />
+}
+function Manager_Base_MyShop_Review_02_02() {
+	return <div className="h-full w-full border-[5px] border-green-300" />
+}
