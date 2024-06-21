bf57444f8751b0455cfe80756b42cb9832bd62e6
snkchan <144839872+snkchan@users.noreply.github.com>
2024-06-21 19:41:19 +0900

    🎨 review요청사항 반영 수정
    * issue: [NAILCASE-161](https://nailcase.atlassian.net/browse/NAILCASE-161)
    sub-issues:
    - [NAILCASE-171](https://nailcase.atlassian.net/browse/NAILCASE-171)

--- src/component/custom/manager/base/schedule/layout/01/index.tsx
+++ src/component/custom/manager/base/schedule/layout/01/index.tsx
@@ -8,7 +8,7 @@ import { usePulldown } from "@/hook/use-component"
 
 export default function ScheduleLayout() {
 	return (
-		<div className="grid w-full grid-rows-[70px_76px_62px] items-center border-t-[1px] border-t-Gray10">
+		<div className="grid w-full grid-rows-[70px_76px_62px] items-center border-t border-t-Gray10">
 			<p className="text-Title03 font-SemiBold">일정</p>
 			<ScheduleController />
 			<ScheduleInfo />
@@ -21,7 +21,7 @@ function ScheduleController() {
 	const month = dayjs().month() + 1
 
 	return (
-		<div className="1 flex h-full w-full items-center justify-between border-t-[1.5px] border-t-Gray10 bg-BGblue01">
+		<div className="flex h-full w-full items-center justify-between border-t-[1.5px] border-t-Gray10 bg-BGblue01">
 			<div className="gap-x- flex items-center">
 				<NTIcon icon="expandLeft" className="h-7 w-7 text-Gray08" />
 				<p className="text-Headline02 text-Gray100">{`${year}년 ${month}월`}</p>
@@ -51,12 +51,18 @@ function ScheduleInfo() {
 					<p className="text-Headline02 font-SemiBold text-Gray100">총 시술</p>
 					<p className="text-Headline01 text-PB100">136 건</p>
 				</div>
-				<NTIcon icon="dot" className="flex h-fit w-7 items-end text-Gray40" />
+				<NTIcon
+					icon="dot"
+					className="flex h-fit w-7 cursor-pointer items-end text-Gray40"
+				/>
 				<div className="flex h-fit items-end gap-x-2">
 					<p className="text-Headline02 text-Gray100">예약 대기</p>
 					<p className="text-Headline01 text-PB100">3 건</p>
 				</div>
-				<NTIcon icon="dot" className="flex h-fit w-7 items-end text-Gray40" />
+				<NTIcon
+					icon="dot"
+					className="flex h-fit w-7 cursor-pointer items-end text-Gray40"
+				/>
 				<div className="flex h-fit items-end gap-x-2">
 					<p className="text-Headline02 text-Gray100">예약 확정</p>
 					<p className="text-Headline01 text-PB100">22 건</p>
