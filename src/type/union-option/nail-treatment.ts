d2ce7ad1c460646e6f0cf724a5222f5d8bd16529
yunshin <2younsin@gmail.com>
2024-06-22 08:31:17 +0900

    ✨ 예약목록 조회 hook 생성
    * issue: [NAILCASE-183](https://nailcase.atlassian.net/browse/NAILCASE-183)
    sub-issues:
     - [NAILCASE-188](https://nailcase.atlassian.net/browse/NAILCASE-188)

---
+++ src/type/union-option/nail-treatment.ts
@@ -0,0 +1,5 @@
+export type TNailTreatment =
+	| "AOM" // 이달의 아트
+	| "CARE" // 케어
+	| "ONE" // 원컬러
+	| "PICTURE" // 사진 등록
---
+++ src/type/union-option/resesrvation-status.ts
@@ -0,0 +1,5 @@
+export type TReservationStatus =
+	| "PENDING" // 예약 대기
+	| "CANCELED" // 예약 취소, 소비자 주도
+	| "REJECTED" // 예약 거절, 판매자 주도
+	| "CONFIRMED" // 예약 승인
