67512321d7eb91cc5c5ca86e692e51dc06fd82cb
snkchan <144839872+snkchan@users.noreply.github.com>
2024-06-21 23:04:54 +0900

    🎨 my-shop/photo/01-01-02
    * issue: [NAILCASE-140](https://nailcase.atlassian.net/browse/NAILCASE-140)
    sub-issues:
    - [NAILCASE-177](https://nailcase.atlassian.net/browse/NAILCASE-177)
    desc:
    - photo/01-01-02 이미지list 작업

--- src/component/custom/manager/base/my-shop/photo/01/index.tsx
+++ src/component/custom/manager/base/my-shop/photo/01/index.tsx
@@ -4,7 +4,7 @@ import { NTButton } from "@/component/common/atom/nt-button"
 
 export default function Manager_Base_MyShop_Photo_01() {
 	return (
-		<div className="flex h-fit w-full flex-col justify-between gap-[30px] border-[5px] border-orange-300">
+		<div className="flex h-fit w-full flex-col justify-between gap-[30px]">
 			<ArtOfTheMonth />
 			<hr className="h-[1.5px] w-full bg-Gray10" />
 			<Manager_Base_MyShop_Photo_01_02 />
@@ -14,16 +14,16 @@ export default function Manager_Base_MyShop_Photo_01() {
 
 function ArtOfTheMonth() {
 	return (
-		<div className="flex h-[215px] w-full justify-between gap-[28px]">
+		<div className="flex h-[215px] w-full justify-between gap-x-[28px]">
 			<ArtOfTheMonthDashboard />
-			<ArtOfTheMonthImages />
+			<ArtOfTheMonthImageList />
 		</div>
 	)
 }
 function ArtOfTheMonthDashboard() {
 	const month = dayjs().month() + 1
 	return (
-		<div className="flex h-full w-[175px] flex-col">
+		<div className="flex h-full min-w-[176px] flex-col">
 			<h2 className="py-3 pl-1 text-Title03 font-SemiBold">이달의 아트</h2>
 			<div className="flex h-full w-full flex-col justify-between">
 				<div className="pl-1">
@@ -38,25 +38,37 @@ function ArtOfTheMonthDashboard() {
 	)
 }
 
-function ArtOfTheMonthImages() {
+function ArtOfTheMonthImageList() {
 	return (
-		<div className="flex h-full w-full flex-col gap-[20px]">
+		<div className="flex h-full w-full flex-col">
 			<ShowAllImages />
-			<Manager_Base_MyShop_Photo_01_01_02_02 />
+			<ArtOfTheMonthImages />
 		</div>
 	)
 }
 function ShowAllImages() {
 	return (
-		<div className="flex h-[30px] w-full flex-col gap-[20px] border-[5px] border-purple-300"></div>
+		<div className="flex w-full justify-end py-3 pr-7 text-Body02 font-SemiBold text-Gray30">
+			전체보기
+		</div>
 	)
 }
-function Manager_Base_MyShop_Photo_01_01_02_02() {
+function ArtOfTheMonthImages() {
 	return (
-		<div className="flex h-full w-full flex-col gap-[20px] border-[5px] border-purple-300"></div>
+		<div className="flex h-full w-fit gap-x-6 pt-[6px]">
+			<ImageBox />
+			<ImageBox />
+			<ImageBox />
+			<ImageBox />
+			<ImageBox />
+		</div>
 	)
 }
-
+function ImageBox() {
+	return (
+		<div className="h-[164.85px] w-[180px] rounded-[26px] border drop-shadow" />
+	)
+}
 function Manager_Base_MyShop_Photo_01_02() {
 	return (
 		<div className="flex h-[215px] w-full justify-between gap-[28px] border-[5px] border-green-300">
