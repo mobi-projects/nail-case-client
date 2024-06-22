ae14c3cba8f4313256b7c9d905c4cf8eacea7e80
keeprok <140785214+keeprok@users.noreply.github.com>
2024-06-23 02:39:34 +0900

    🎨 manager/base/my/shop/review/03-01-04
    * issue: [NAILCASE-139](https://nailcase.atlassian.net/browse/NAILCASE-139)
    sub-issues:
    - [NAILCASE-175](https://nailcase.atlassian.net/browse/NAILCASE-175)

--- src/component/custom/manager/base/my-shop/review/03/index.tsx
+++ src/component/custom/manager/base/my-shop/review/03/index.tsx
@@ -1,11 +1,15 @@
+import { NTButton } from "@/component/common/atom/nt-button"
+
 export default function Manager_Base_MyShop_Review_03() {
 	return (
 		<div className="mt-[18px] flex w-full flex-col gap-[25px] border-[5px] border-orange-300">
 			<Manager_Base_MyShop_Review_03_01 />
 			<Manager_Base_MyShop_Review_03_01 />
 			<Manager_Base_MyShop_Review_03_01 />
+			<ManagerResponse />
 			<Manager_Base_MyShop_Review_03_01 />
 			<Manager_Base_MyShop_Review_03_01 />
+			<ManagerResponse />
 		</div>
 	)
 }
@@ -40,5 +44,32 @@ function Manager_Base_MyShop_Review_03_01_03_01() {
 function Manager_Base_MyShop_Review_03_01_03_02() {
 	return (
 		<div className="flex h-fit min-h-[65px] w-[630px] border-[5px] border-purple-300"></div>
+	)
+}
+function ManagerResponse() {
+	return (
+		<div className="flex h-fit min-h-[178.98px] w-full justify-between rounded-[26px] pt-[18.5px] shadow-customGray60">
+			<div className="ml-[36px] flex flex-col gap-[18px] pt-[6px]">
+				<div className="flex items-center gap-[12px]">
+					<div className="text-Headline02 font-SemiBold text-Gray80">
+						모비네일 원장 모비
+					</div>
+					<div className="flex text-Callout text-Gray50">이번주</div>
+				</div>
+				<div className="line-clamp-5 text-Title03 font-Regular text-Gray80">
+					다음에 오시면 더 잘해드릴게요
+					<br />
+					리뷰 남겨주셔서 감사합니다 ㅎㅎ
+				</div>
+			</div>
+			<div className="mr-[21px] flex flex-col gap-[16px]">
+				<NTButton variant="secondary" size="large">
+					수정하기
+				</NTButton>
+				<NTButton variant="secondary" size="large">
+					삭제하기
+				</NTButton>
+			</div>
+		</div>
 	)
 }
