6dd183645cb53f34bd97f6a343be7a73734f6f2d
keeprok <140785214+keeprok@users.noreply.github.com>
2024-06-22 17:57:57 +0900

    🎨 manager/base/my-shop/review03-01-03-01
    * issue: [NAILCASE-139](https://nailcase.atlassian.net/browse/NAILCASE-139) sub-issues: - [NAILCASE-175](https://nailcase.atlassian.net/browse/NAILCASE-175)

--- src/component/custom/manager/base/my-shop/review/03/index.tsx
+++ src/component/custom/manager/base/my-shop/review/03/index.tsx
@@ -26,19 +26,29 @@ function Manager_Base_MyShop_Review_03_01_02() {
 }
 function Manager_Base_MyShop_Review_03_01_03() {
 	return (
-		<div className="flex h-fit min-h-[65px] w-full gap-[36px] border-[5px] border-blue-300">
-			<Manager_Base_MyShop_Review_03_01_03_01 />
-			<Manager_Base_MyShop_Review_03_01_03_02 />
+		<div className="flex h-fit min-h-[30px] w-full items-end justify-between pr-[21px]">
+			<div className="flex w-full gap-[36px]">
+				<ReivewCommentImgae />
+				<ReivewComment />
+			</div>
 		</div>
 	)
 }
-function Manager_Base_MyShop_Review_03_01_03_01() {
-	return (
-		<div className="flex h-[165px] w-[180px] border-[5px] border-purple-300"></div>
-	)
+function ReivewCommentImgae() {
+	return <div></div>
 }
-function Manager_Base_MyShop_Review_03_01_03_02() {
+
+function ReivewComment() {
 	return (
-		<div className="flex h-fit min-h-[65px] w-[630px] border-[5px] border-purple-300"></div>
+		<div className="line-clamp-5 h-fit w-[631px] text-Title03 text-Gray80">
+			젠가 그대가 원한다면 Only we’ve been seeing what we need my bad 우리에게
+			필요한 건 우리만 아니까 If you’re sober I’ll feel fine 그대가 취하지
+			않았다면 난 괜찮을 거예요 If we get away, could we be like that? 만약 함께
+			떠난다면 우리에게 사랑이 올까요? Sometime if you want me like that 언젠가
+			그대가 원한다면 Only we’ve been seeing what we need my bad 우리에게 필요한
+			건 우리만 아니까 If you’re sober I’ll feel fine 그대가 취하지 않았다면 난
+			괜찮을 거예요 Oh oh oh oh (give it to me) 오 오 오 오 (신호를 줘요) Oh oh
+			oh oh (give it to me) 오 오 오 오 (내가 알 수 있도록) Oh oh oh o
+		</div>
 	)
 }
