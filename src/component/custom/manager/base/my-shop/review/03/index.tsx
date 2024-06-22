4fb11e05ef3d24a22b439db4d672b9f3ba018b8c
keeprok <140785214+keeprok@users.noreply.github.com>
2024-06-22 16:35:22 +0900

    🎨 manager/base/my-shop/review/03-01-02
    * issue: [NAILCASE-139](https://nailcase.atlassian.net/browse/NAILCASE-139)
    sub-issues:
    - [NAILCASE-175](https://nailcase.atlassian.net/browse/NAILCASE-175)

--- src/component/custom/manager/base/my-shop/review/03/index.tsx
+++ src/component/custom/manager/base/my-shop/review/03/index.tsx
@@ -13,7 +13,7 @@ function Manager_Base_MyShop_Review_03_01() {
 	return (
 		<div className="flex w-full flex-col gap-[16px] border-[5px] border-green-300">
 			<Manager_Base_MyShop_Review_03_01_01 />
-			<Manager_Base_MyShop_Review_03_01_02 />
+			<ReviewerTag />
 			<Manager_Base_MyShop_Review_03_01_03 />
 		</div>
 	)
@@ -21,8 +21,25 @@ function Manager_Base_MyShop_Review_03_01() {
 function Manager_Base_MyShop_Review_03_01_01() {
 	return <div className="h-[60px] w-full border-[5px] border-blue-300"></div>
 }
-function Manager_Base_MyShop_Review_03_01_02() {
-	return <div className="h-[60px] w-full border-[5px] border-blue-300"></div>
+function ReviewerTag() {
+	const tagList = [
+		"이달의 아트",
+		"동반 2인",
+		"타샵 제거 있음",
+		" 1인 연장 필요",
+	]
+	return (
+		<div className="flex h-[60px] w-full gap-[10px] pt-[3px]">
+			{tagList.map((tag, idx) => (
+				<div
+					key={idx}
+					className="h-fit w-fit rounded-[20.34px] bg-BGblue01 px-[18px] py-[12px] text-Body02 text-PB100"
+				>
+					{tag}
+				</div>
+			))}
+		</div>
+	)
 }
 function Manager_Base_MyShop_Review_03_01_03() {
 	return (
