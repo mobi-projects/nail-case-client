906adc28a4907f16858585239f5664e7c3a95824
keeprok <140785214+keeprok@users.noreply.github.com>
2024-06-22 13:42:35 +0900

    design : manager/base/my-shop/review/02-01
    * issue: [NAILCASE-139](https://nailcase.atlassian.net/browse/NAILCASE-139)
    sub-issues:
    - [NAILCASE-175](https://nailcase.atlassian.net/browse/NAILCASE-175)

--- src/component/custom/manager/base/my-shop/review/02/index.tsx
+++ src/component/custom/manager/base/my-shop/review/02/index.tsx
@@ -1,13 +1,35 @@
-export default function Manager_Base_MyShop_Review_02() {
+"use client"
+import { useState } from "react"
+
+export default function ReviewTitleheader() {
 	return (
-		<div className="flex h-[50px] w-full items-center justify-between border-[5px] border-orange-300">
-			<Manager_Base_MyShop_Review_02_01 />
+		<div className="flex h-[50px] w-full items-center justify-between">
+			<ReviewTitleList />
 			<Manager_Base_MyShop_Review_02_02 />
 		</div>
 	)
 }
-function Manager_Base_MyShop_Review_02_01() {
-	return <div className="h-full w-full border-[5px] border-green-300" />
+function ReviewTitleList() {
+	const reviewTitle = ["전체", "최신순", "사진리뷰"]
+	const [selectedIdx, setSelectedIdx] = useState(0)
+
+	const handleClick = (idx: number) => {
+		setSelectedIdx(idx)
+	}
+
+	return (
+		<div className="flex w-full gap-[18px] text-Gray50">
+			{reviewTitle.map((item, idx) => (
+				<div
+					className={`cursor-pointer text-Title03 font-SemiBold ${selectedIdx === idx ? "text-PB100" : "text-Gray50"}`}
+					key={idx}
+					onClick={() => handleClick(idx)}
+				>
+					{item}
+				</div>
+			))}
+		</div>
+	)
 }
 function Manager_Base_MyShop_Review_02_02() {
 	return <div className="h-full w-full border-[5px] border-green-300" />
