43a4cc8222c22c279987e5d73c309cc2ac7b1bf1
yunshin <2younsin@gmail.com>
2024-06-22 23:07:39 +0900

    🎨 소개글 수정 모달 퍼블리싱
    * issue: [NAILCASE-140](https://nailcase.atlassian.net/browse/NAILCASE-140)
    sub-issues:
     - [NAILCASE-176](https://nailcase.atlassian.net/browse/NAILCASE-176)

---
+++ src/component/custom/manager/base/my-shop/layout/modal/01/index.tsx
@@ -0,0 +1,56 @@
+import { NTButton } from "@/component/common/atom/nt-button"
+import {
+	NTModalContent,
+	NTModalFooter,
+	NTModalHeader,
+} from "@/component/common/nt-modal"
+import NTSearchfield from "@/component/common/nt-searchfield"
+
+export default function EditIntroduction() {
+	return (
+		<div className="flex h-full w-full flex-col gap-2">
+			<NTModalHeader>소개글</NTModalHeader>
+
+			<NTModalContent className="flex h-fit flex-col gap-[28px]">
+				<NTSearchfield />
+				<TextArea tags={["#qdsfa", "#sdfsfd"]} />
+			</NTModalContent>
+
+			<NTModalFooter className="h-fit">
+				<NTButton>저장하기</NTButton>
+			</NTModalFooter>
+		</div>
+	)
+}
+
+type TextAreaPT = {
+	tags?: string[]
+}
+
+function TextArea({ tags = [] }: TextAreaPT) {
+	return (
+		<div className="relative flex h-[200px] w-full flex-col gap-[10px] rounded-[22px] border-[1.6px] border-Gray30 bg-White p-[20px] pt-[10px] outline-none">
+			<div className="flex h-fit w-full flex-wrap gap-[5px] text-nowrap">
+				{tags.map((tag, idx) => (
+					<p key={idx} className="text-Title02 text-Gray90">
+						{tag}
+					</p>
+				))}
+			</div>
+			<textarea className="h-[70%] w-full resize-none text-Body01 text-Gray90 focus-visible:outline-none" />
+			<TextCounter length={10} />
+		</div>
+	)
+}
+
+type TextCounterPT = {
+	length?: number
+}
+
+function TextCounter({ length = 0 }: TextCounterPT) {
+	return (
+		<p className="absolute bottom-[5px] right-[20px] w-full text-end text-Callout text-Gray50">
+			{length} 자
+		</p>
+	)
+}
