9706392ff7ffac2ffbf9b1e89beb9cd882300010
snkchan <144839872+snkchan@users.noreply.github.com>
2024-06-24 04:02:02 +0900

    🎨  photo/01-02
    * issue: [NAILCASE-140](https://nailcase.atlassian.net/browse/NAILCASE-140)
    sub-issues:
    - [NAILCASE-177](https://nailcase.atlassian.net/browse/NAILCASE-177)

--- src/component/custom/manager/base/my-shop/photo/01/index.tsx
+++ src/component/custom/manager/base/my-shop/photo/01/index.tsx
@@ -2,12 +2,12 @@ import dayjs from "dayjs"
 
 import { NTButton } from "@/component/common/atom/nt-button"
 
-export default function Manager_Base_MyShop_Photo_01() {
+export default function RegisterImages() {
 	return (
 		<div className="flex h-fit w-full flex-col justify-between gap-[30px]">
 			<ArtOfTheMonth />
 			<hr className="h-[1.5px] w-full bg-Gray10" />
-			<Manager_Base_MyShop_Photo_01_02 />
+			<ShopCoverImage />
 		</div>
 	)
 }
@@ -69,32 +69,46 @@ function ImageBox() {
 		<div className="h-[164.85px] w-[180px] rounded-[26px] border drop-shadow" />
 	)
 }
-function Manager_Base_MyShop_Photo_01_02() {
+function ShopCoverImage() {
 	return (
-		<div className="flex h-[215px] w-full justify-between gap-[28px] border-[5px] border-green-300">
-			<Manager_Base_MyShop_Photo_01_02_01 />
-			<Manager_Base_MyShop_Photo_01_02_02 />
+		<div className="mb-2 flex h-[215px] w-full justify-between gap-x-[28px]">
+			<ShopCoverImageDashBorad />
+			<ShopCoverImageImageList />
 		</div>
 	)
 }
-function Manager_Base_MyShop_Photo_01_02_01() {
-	return <div className="h-full w-[175px] border-[5px] border-blue-300" />
-}
-function Manager_Base_MyShop_Photo_01_02_02() {
+function ShopCoverImageDashBorad() {
 	return (
-		<div className="flex h-full w-full flex-col gap-[20px] border-[5px] border-blue-300">
-			<Manager_Base_MyShop_Photo_01_02_02_01 />
-			<Manager_Base_MyShop_Photo_01_02_02_02 />
+		<div className="flex h-full min-w-[176px] flex-col">
+			<h2 className="py-3 pl-1 text-Title03 font-SemiBold">대표 사진</h2>
+			<div className="flex h-full w-full flex-col justify-between">
+				<div className="pl-1">
+					<p className="text-Title03 font-SemiBold text-PB100">4개</p>
+				</div>
+				<NTButton icon="addRoundLight" variant={"tertiary"} size={"large"}>
+					사진 올리기
+				</NTButton>
+			</div>
 		</div>
 	)
 }
-function Manager_Base_MyShop_Photo_01_02_02_01() {
+
+function ShopCoverImageImageList() {
 	return (
-		<div className="flex h-[30px] w-full flex-col gap-[20px] border-[5px] border-purple-300"></div>
+		<div className="flex h-full w-full flex-col">
+			<ShowAllImages />
+			<ShopCoverImages />
+		</div>
 	)
 }
-function Manager_Base_MyShop_Photo_01_02_02_02() {
+function ShopCoverImages() {
 	return (
-		<div className="flex h-full w-full flex-col gap-[20px] border-[5px] border-purple-300"></div>
+		<div className="flex h-full w-fit gap-x-6 pt-[6px]">
+			<ImageBox />
+			<ImageBox />
+			<ImageBox />
+			<ImageBox />
+			<ImageBox />
+		</div>
 	)
 }
