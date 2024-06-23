4a247e997f89e91a877ecf6450870f527300b889
yunshin <2younsin@gmail.com>
2024-06-24 01:19:24 +0900

    🚚 manager/base/layout.tsx 파일 위치 이동
    * issue: [NAILCASE-137](https://nailcase.atlassian.net/browse/NAILCASE-137)
    sub-issues:
     - [NAILCASE-166](https://nailcase.atlassian.net/browse/NAILCASE-166)
    
    desc:
     - `app > manager > layout` -> `app > manager > base > layout`

--- src/component/custom/manager/base/layout/01/index.tsx
+++ src/component/custom/manager/base/layout/01/index.tsx
@@ -6,7 +6,7 @@ import NTLogo from "@/../public/asset/nt-logo.svg"
 import NTIcon from "@/component/common/nt-icon"
 import NTSearchfield from "@/component/common/nt-searchfield"
 
-export default function ManagerLayout() {
+export default function ManagerBaseHeader() {
 	return (
 		<div className="flex h-fit w-full flex-col gap-[8.5px] pb-[14px] pt-[68px]">
 			<Image src={NTLogo} alt="brand-logo" width={134} height={38} priority />
