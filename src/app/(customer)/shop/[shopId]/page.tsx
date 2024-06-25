604c66e7cddc6c02705bb70f3b1b68c54402f9f9
yunshin <2younsin@gmail.com>
2024-06-25 18:32:57 +0900

    🎨 customer/shop/02 publishing
    * issue: [NAILCASE-199](https://nailcase.atlassian.net/browse/NAILCASE-199)
    sub-issues:
      - [NAILCASE-201](https://nailcase.atlassian.net/browse/NAILCASE-201)

--- src/app/(customer)/shop/[shopId]/page.tsx
+++ src/app/(customer)/shop/[shopId]/page.tsx
@@ -1,5 +1,5 @@
 import Customer_Shop_01 from "@/component/custom/customer/shop/01"
-import Customer_Shop_02 from "@/component/custom/customer/shop/02"
+import ReservationSchedule from "@/component/custom/customer/shop/02"
 import { Customer_Shop_03 } from "@/component/custom/customer/shop/03"
 import Customer_Shop_04 from "@/component/custom/customer/shop/04"
 import Customer_Shop_05 from "@/component/custom/customer/shop/05"
@@ -10,13 +10,14 @@ type CustomerShopPT = {
 	}
 }
 export default function CustomerShop({ params }: CustomerShopPT) {
+	console.log(params) // [TODO] 추후 삭제: 미사용 변수가 잔여할 경우, 배포 불가 이유로 console 출력 중..
 	return (
 		<div className="h-full w-full">
 			<Customer_Shop_01 />
 			<div className="mt-[32px] flex w-full flex-col gap-[32px]">
 				<div className="flex w-full flex-col gap-[20px]">
 					<p className="text-Title02">예약 일시</p>
-					<Customer_Shop_02 />
+					<ReservationSchedule />
 				</div>
 				<Customer_Shop_03 />
 				<Customer_Shop_04 />
--- src/component/custom/customer/shop/02/index.tsx
+++ src/component/custom/customer/shop/02/index.tsx
@@ -1,19 +1,79 @@
-export default function Customer_Shop_02() {
+"use client"
+
+import { NTButton } from "@/component/common/atom/nt-button"
+import NTOption from "@/component/common/nt-option"
+import { useOption } from "@/hook/use-component"
+
+export default function ReservationSchedule() {
 	return (
-		<div className="grid h-[410px] w-full grid-cols-[690px_485px] justify-between border-[5px] border-orange-300">
-			<Customer_Shop_02_01 />
-			<Customer_Shop_02_02 />
+		<div className="grid h-fit w-full grid-cols-[690px_485px] justify-between">
+			<Calendar />
+			<ReservationCheck />
 		</div>
 	)
 }
-function Customer_Shop_02_01() {
+function Calendar() {
 	return (
-		<div className="flex w-full flex-col justify-between gap-[20px] border-[5px] border-green-300"></div>
+		<div className="grid h-full w-full grid-rows-[326.8px_auto] gap-[21px]">
+			<CalendarBody />
+			<NTButton variant="tertiary" flexible="full" size="small">
+				예약하기
+			</NTButton>
+		</div>
 	)
 }
-
-function Customer_Shop_02_02() {
+function ReservationCheck() {
 	return (
-		<div className="flex w-full flex-col justify-between gap-[20px] border-[5px] border-green-300"></div>
+		<div className="grid h-full w-full grid-rows-[326.8px_auto] gap-[18px]">
+			<ReservationCheckBody />
+			<NTButton variant="tertiary" flexible="full" size="small">
+				전화하기
+			</NTButton>
+		</div>
+	)
+}
+function CalendarBody() {
+	return (
+		<div className="flex h-full w-full items-center justify-center rounded-[26px] border-[5px] border-green-300 drop-shadow">
+			🛠️ 달력 준비 중..
+		</div>
+	)
+}
+function ReservationCheckBody() {
+	const { checkedOption, onClickOption, optionArr } = useOption([
+		"오전 11시",
+		"오후 1시",
+		"오후 2시",
+		"오후 3시",
+		"오전 11시",
+		"오후 1시",
+		"오후 3시",
+	])
+	const {
+		checkedOption: checkedOption3,
+		onClickOption: onClickOption3,
+		optionArr: optionArr3,
+	} = useOption(["1인", "2인 동반"])
+
+	return (
+		<div className="gap-[16px]] flex h-full w-full flex-col justify-around py-[10px]">
+			<p className="w-full justify-center text-center text-[1rem] font-SemiBold text-Gray90">
+				6월 27일 (목요일)
+			</p>
+			<hr />
+			<NTOption
+				{...{ checkedOption, optionArr, onClickOption }}
+				itemsPerRow={4}
+			/>
+			<hr />
+			<NTOption
+				checkedOption={checkedOption3}
+				optionArr={optionArr3}
+				onClickOption={onClickOption3}
+			/>
+			<p className="text-[16px] font-Regular text-Gray50">
+				2인 동반 선택시에는 두 타임 예약 부탁드립니다 :)
+			</p>
+		</div>
 	)
 }
