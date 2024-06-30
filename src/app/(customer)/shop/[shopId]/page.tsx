50547e3651114572384b7858982325505eb139b3
yunshin <2younsin@gmail.com>
2024-06-30 14:00:29 +0900

    🎨 customer/shop/01
    * issue: [NAILCASE-199](https://nailcase.atlassian.net/browse/NAILCASE-199)
    sub-issues:
     - [NAILCASE-201](https://nailcase.atlassian.net/browse/NAILCASE-201)

--- src/app/(customer)/shop/[shopId]/page.tsx
+++ src/app/(customer)/shop/[shopId]/page.tsx
@@ -1,4 +1,4 @@
-import Customer_Shop_01 from "@/component/custom/customer/shop/01"
+import ShopShowCase from "@/component/custom/customer/shop/01"
 import ReservationSchedule from "@/component/custom/customer/shop/02"
 import CustomerNaviBar from "@/component/custom/customer/shop/03"
 import ShopInfoCardList from "@/component/custom/customer/shop/04"
@@ -13,7 +13,7 @@ export default function CustomerShop({ params }: CustomerShopPT) {
 	console.log(params) // [TODO] 추후 삭제: 미사용 변수가 잔여할 경우, 배포 불가 이유로 console 출력 중..
 	return (
 		<div className="h-full w-full">
-			<Customer_Shop_01 />
+			<ShopShowCase />
 			<div className="mt-[32px] flex w-full flex-col gap-[32px]">
 				<div className="flex w-full flex-col gap-[20px]">
 					<p className="text-Title02">예약 일시</p>
--- src/component/custom/customer/shop/01/index.tsx
+++ src/component/custom/customer/shop/01/index.tsx
@@ -1,22 +1,64 @@
-export default function Customer_Shop_01() {
+"use client"
+import BannerCarousel from "@/component/common/nt-banner-carousel"
+import NTContent from "@/component/common/nt-content"
+import { useShopInfo } from "@/hook/use-common"
+import { useBanner } from "@/hook/use-component"
+import type { TShopInfo } from "@/type"
+
+type BannerItemPT = {
+	shopInfo: TShopInfo
+}
+
+export default function ShopShowCase() {
+	const { handleCarousel, carouselIdx } = useBanner()
+	const { shopInfo } = useShopInfo()
+	if (!shopInfo) return <h1>Loading Banner....</h1>
 	return (
-		<div className="flex h-[480px] w-full">
-			<div className="absolute left-0 flex h-[480px] w-full items-center justify-center border-[5px] border-orange-300">
-				<div className="flex h-fit w-[1200px] flex-col gap-[50px]">
-					<Customer_Shop_01_01 />
-					<Customer_Shop_01_02 />
-					<Customer_Shop_01_03 />
-				</div>
+		<div className="h-[30rem] w-full">
+			<div className="absolute left-0 top-0 h-full w-full">
+				<BannerCarousel type="user" handleCarousel={handleCarousel}>
+					<BannerHeader shopInfo={shopInfo} />
+					<BannerDesciption shopInfo={shopInfo} />
+					<NTContent
+						mode="dark"
+						className="absolute right-80 top-24"
+					>{`${carouselIdx + 1}/${shopInfo.srcArr.length}`}</NTContent>
+				</BannerCarousel>
 			</div>
 		</div>
 	)
 }
-function Customer_Shop_01_01() {
-	return <div className="h-[36px] w-full border-[5px] border-green-300"></div>
+
+function BannerHeader({ shopInfo }: BannerItemPT) {
+	const { specialty, address, shopName, todayAccess, totalAccess } = shopInfo
+	return (
+		<div className="absolute left-60 top-24 z-10">
+			<p className="text-Callout text-[14px] font-Light text-White">{`${specialty}  |  ${address}`}</p>
+			<h1 className="text-Title01 text-[28px] font-Bold text-White">
+				{shopName}
+			</h1>
+			<p className="text-Callout font-SemiBold text-Gray20">{`오늘 ${todayAccess.toLocaleString("ko-KR")} · 전체 ${totalAccess.toLocaleString("ko-KR")}`}</p>
+		</div>
+	)
 }
-function Customer_Shop_01_02() {
-	return <div className="h-[136px] w-full border-[5px] border-green-300"></div>
-}
-function Customer_Shop_01_03() {
-	return <div className="h-[106px] w-full border-[5px] border-green-300"></div>
+
+function BannerDesciption({ shopInfo }: BannerItemPT) {
+	const { hashtagArr, overview } = shopInfo
+	return (
+		<div className="absolute left-60 top-72 z-10">
+			<div className="flex gap-3">
+				{hashtagArr.map((hashtag, idx) => (
+					<p
+						key={idx}
+						className="text-Body01 text-[18px] font-SemiBold text-White"
+					>
+						{hashtag}
+					</p>
+				))}
+			</div>
+			<p className="line-clamp-3 w-[500px] text-Body01 text-[18px] font-Regular text-Gray10">
+				{overview}
+			</p>
+		</div>
+	)
 }
