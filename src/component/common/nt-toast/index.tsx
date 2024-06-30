2b7f2ef4cb008c571eadb17751e456971fa8fc1b
HeesikK <7jjhkhz@naver.com>
2024-07-01 01:16:59 +0900

    ⚡ ️ToastMessage 코드 수정
    * issue: [NAILCASE-209](https://nailcase.atlassian.net/browse/NAILCASE-209)

--- src/component/common/nt-toast/index.tsx
+++ src/component/common/nt-toast/index.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react"
-
 import { NTButton } from "../atom/nt-button"
 
 type ToastMessagePT = {
@@ -13,36 +11,19 @@ export const ToastMessage: React.FC<ToastMessagePT> = ({
 	time,
 	tags,
 }) => {
-	const [isClicked, setIsClicked] = useState(false)
-
-	const handleChange = () => {
-		setIsClicked((prev) => !prev)
-	}
-
-	const handleButtonClick = (e: React.MouseEvent) => {
-		e.stopPropagation()
-	}
-
 	return (
-		<div
-			onClick={handleChange}
-			className="h-fit w-[450px] rounded-[14px] bg-White px-[20px] py-[20px] shadow-customGray60"
-		>
+		<div className="h-fit w-[450px] rounded-[14px] bg-White px-[20px] py-[20px] shadow-customGray60">
 			<div className="flex items-center justify-between">
 				<div>
 					<p className="text-Headline02 text-PB100">{title}</p>
 					<p className="text-[16px] font-SemiBold text-Gray70">{time}</p>
 				</div>
-				<NTButton onClick={handleButtonClick}>예약 확정</NTButton>
+				<NTButton>예약 확정</NTButton>
 			</div>
-			{isClicked && (
-				<div className="flex items-center justify-between pt-[5px]">
-					<div className="text-Body02 text-Gray70">{tags.join(", ")}</div>
-					<NTButton variant="tertiary" onClick={handleButtonClick}>
-						예약 관리
-					</NTButton>
-				</div>
-			)}
+			<div className="flex items-center justify-between pt-[5px]">
+				<div className="text-Body02 text-Gray70">{tags.join(", ")}</div>
+				<NTButton variant="tertiary">예약 관리</NTButton>
+			</div>
 		</div>
 	)
 }
