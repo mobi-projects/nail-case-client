7857f0e504e3fac1efdddfe18c29378a149b9c38
keeprok <140785214+keeprok@users.noreply.github.com>
2024-07-09 21:18:09 +0900

    🎉 customer base home  작업
    * issue: [NAILCASE-247](https://nailcase.atlassian.net/browse/NAILCASE-247)
    sub-issues:
     - [NAILCASE-281](https://nailcase.atlassian.net/browse/NAILCASE-281)

---
+++ src/app/(customer)/base/(home)/page.tsx
@@ -0,0 +1,13 @@
+export default function CustomerBaseHome() {
+	return (
+		<div className="flex flex-col">
+			<div>배너</div>
+			<div className="flex">
+				<div>진행 중인 네일</div>
+				<div>다시 돌아보는 지난 네일</div>
+			</div>
+			<div>내 주변 네일샵 👣</div>
+			<div>내가 좋아한 네일샵 ✨</div>
+		</div>
+	)
+}
