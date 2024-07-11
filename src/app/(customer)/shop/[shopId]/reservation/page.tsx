572960c9b9bba2f6ead29e8e50c3129c048754ca
yunshin <2younsin@gmail.com>
2024-07-12 03:07:36 +0900

    ✨ "동반인원"만큼의 예약폼 구성
    * issue: [NAILCASE-249](https://nailcase.atlassian.net/browse/NAILCASE-249)
    sub-issues:
     - [NAILCASE-302](https://nailcase.atlassian.net/browse/NAILCASE-302)

--- src/app/(customer)/shop/[shopId]/reservation/page.tsx
+++ src/app/(customer)/shop/[shopId]/reservation/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import { NTButton } from "@/component/common/atom/nt-button"
 import CustomerShopReservationHeader from "@/component/custom/customer/shop/reservation/01"
@@ -8,17 +8,52 @@ import Artist from "@/component/custom/customer/shop/reservation/03"
 import TreatmentNCondition from "@/component/custom/customer/shop/reservation/04"
 import ScheduleSelection from "@/component/custom/customer/shop/reservation/05"
 import { ExpandableToggle } from "@/component/custom/customer/shop/reservation/common/expandable-toggle"
+import type { TNailCondition } from "@/type/union-option/nail-condition"
+import type { TNailTreatment } from "@/type/union-option/nail-treatment"
+import type { TRemoveOption } from "@/type/union-option/remove-option"
 
 type CustomerShopPT = {
 	params: {
 		shopId: number
 	}
 }
+export type TReservationForm = {
+	shopId: number
+	startTime: number
+	remove: TRemoveOption
+	extend: boolean
+	conditionList: {
+		option: TNailCondition
+	}[]
+	treatmentList: {
+		option: TNailTreatment
+	}[]
+}
+const initialReservationForm: TReservationForm = {
+	shopId: -1,
+	startTime: -1,
+	remove: "NO_NEED",
+	extend: false,
+	conditionList: [],
+	treatmentList: [],
+}
 
 export default function CustomerShopReservation({ params }: CustomerShopPT) {
 	const { shopId } = params
 	const [companion, setCompanion] = useState(1)
 	const [artistIdArr, setArtistIdArr] = useState<number[]>([])
+	const [reservationFormArr, setReservationFormArr] = useState<
+		TReservationForm[]
+	>([initialReservationForm])
+
+	useEffect(() => {
+		setReservationFormArr((prev) => {
+			const _prev = [...prev]
+			while (_prev.length < companion) _prev.push(initialReservationForm)
+			while (_prev.length > companion) _prev.pop()
+			return _prev
+		})
+	}, [companion])
 
 	return (
 		<main className="h-fit w-full">
@@ -38,7 +73,9 @@ export default function CustomerShopReservation({ params }: CustomerShopPT) {
 					/>
 				</ExpandableToggle>
 				<ExpandableToggle title="시술 세부 내용">
-					<TreatmentNCondition />
+					<TreatmentNCondition
+						{...{ companion, reservationFormArr, setReservationFormArr }}
+					/>
 				</ExpandableToggle>
 				<ExpandableToggle title="시술 일정">
 					<ScheduleSelection shopId={shopId} />
