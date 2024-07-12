b56419f5defba4fb9d72070ef8339a8a043d854d
snkchan <144839872+snkchan@users.noreply.github.com>
2024-07-13 04:34:44 +0900

    🎨 로그인 페이지에서 보여질 문구
    * issue: [NAILCASE-269](https://nailcase.atlassian.net/browse/NAILCASE-269)
    sub-issues:
    - [NAILCASE-288](https://nailcase.atlassian.net/browse/NAILCASE-288)

---
+++ src/component/custom/customer/sign/03/indext.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link"
+
+import { isCustomer } from "@/app/(customer)/sign/[type]/page"
+import { COMMON_SIGN } from "@/constant/routing-path"
+import type { TSignType } from "@/type/union-option/sign-type"
+
+type CheckMessaPT = { loginType: TSignType }
+
+export default function CheckMessage({ loginType }: CheckMessaPT) {
+	const anotherLoginPath = changePath(loginType)
+	const AlertMessage = isCustomer(loginType)
+		? "아티스트로 로그인하여 고객을 만나보려면"
+		: " NewTips에서 네일 예약을 원하신다면 사용자 계정으로 로그인하세요!"
+
+	return (
+		<p className="group mb-2 text-center text-Body02 font-Regular text-Gray50">
+			{AlertMessage}
+			<Link href={`${COMMON_SIGN}/${anotherLoginPath}`}>
+				<span className="px-1 font-SemiBold text-Gray80 group-hover:underline">
+					여기
+				</span>
+			</Link>
+			를 눌러주세요.
+		</p>
+	)
+}
+
+const changePath = (path: TSignType): string => {
+	if (isCustomer(path)) return "manager"
+	return "member"
+}
