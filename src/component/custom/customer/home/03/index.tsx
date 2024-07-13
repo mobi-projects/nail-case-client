10cb2efa5ea4341fb5eac0b2ed4487a906a2263c
keeprok <140785214+keeprok@users.noreply.github.com>
2024-07-14 05:30:54 +0900

    🎨 main page 데이터 추가 패칭 디자인 변경
    * issue: [NAILCASE-247](https://nailcase.atlassian.net/browse/NAILCASE-247)
    sub-issues:
    - [NAILCASE-251](https://nailcase.atlassian.net/browse/NAILCASE-251)

--- src/component/custom/customer/home/03/index.tsx
+++ src/component/custom/customer/home/03/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 
 import NTIcon from "@/component/common/nt-icon"
+import { cn } from "@/config/tailwind"
 import type { TResShop } from "@/type/main-page"
 
 import type { TShop } from "./type"
@@ -8,14 +9,16 @@ import type { TShop } from "./type"
 type ShopListFomrPT = {
 	listData: Array<TResShop>
 	listMockData: Array<TShop>
+	formTitle: string
 }
 export default function ShopListForm({
 	listData = [],
 	listMockData,
+	formTitle,
 }: ShopListFomrPT) {
 	return (
 		<div className="flex h-fit w-fit flex-col gap-[20px]">
-			<ShopListTitle />
+			<ShopListTitle formTitle={formTitle} />
 			<div className="flex gap-[24px]">
 				{listData.map((data, idx) => {
 					return <ShopForm key={idx} data={data} mockData={listMockData[idx]} />
@@ -24,11 +27,13 @@ export default function ShopListForm({
 		</div>
 	)
 }
-function ShopListTitle() {
+type ShopListTitlePT = {
+	formTitle: string
+}
+function ShopListTitle({ formTitle }: ShopListTitlePT) {
 	return (
 		<div className="flex w-full justify-between pr-[20.5px]">
-			<div className="text-Title03 font-SemiBold">내가 좋아한 네일샵 ✨</div>
-			<div className="text-Headline02 text-Gray40">전체보기</div>
+			<div className="text-Title03 font-SemiBold">{formTitle}</div>
 		</div>
 	)
 }
@@ -49,34 +54,40 @@ function ShopForm({ data, mockData }: ShopFormPT) {
 				/>
 				<NTIcon
 					icon="favoriteFill"
-					className={`absolute right-[18px] top-[21px] text-Headline01 ${mockData.liked ? "text-PY100" : "text-White"}`}
+					className={cn(
+						"absolute right-[18px] top-[21px] text-Headline01 text-White",
+						data.likedByUser && "text-PY100",
+					)}
 				/>
 				<div
-					className={`absolute inset-0 rounded-[26px] bg-gradient-to-tr from-Black to-White opacity-0 transition-opacity duration-300 ${mockData.liked ? "group-hover:opacity-30" : "group-hover:opacity-60"}`}
-				></div>
-				<ShopHoverInfo data={data} mockData={mockData} />
+					className={cn(
+						"absolute inset-0 rounded-[26px] bg-gradient-to-tr from-Black to-White opacity-0 transition-opacity duration-300 group-hover:opacity-60",
+						data.likedByUser && "group-hover:opacity-30",
+					)}
+				/>
+				<ShopHoverInfo data={data} />
 			</div>
 			<ShopFormInfo data={data} />
 		</div>
 	)
 }
-function ShopHoverInfo({ data, mockData }: ShopFormPT) {
+function ShopHoverInfo({ data }: ShopHoverInfoPT) {
 	return (
 		<div className="absolute inset-0 flex flex-col justify-between py-[19px] pl-[29px] text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
 			<div className="pt-[10]">
 				<div className="text-Headline01 text-PY100">{data.shopName}</div>
 				<div className="max-w-[300px] whitespace-pre-wrap text-Body01 font-SemiBold">
-					{mockData.content}
+					{data.overview}
 				</div>
 			</div>
 			<div className="hover:text-Gray60">예약하러가기</div>
 		</div>
 	)
 }
-type ShopFormInfoPT = {
+type ShopHoverInfoPT = {
 	data: TResShop
 }
-function ShopFormInfo({ data }: ShopFormInfoPT) {
+function ShopFormInfo({ data }: ShopHoverInfoPT) {
 	return (
 		<div>
 			<div className="text-Body01 font-SemiBold">{data.shopName}</div>
