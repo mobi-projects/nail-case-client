d3b25fc6ad2c29965684aec584f6882d4eca031d
keeprok <140785214+keeprok@users.noreply.github.com>
2024-07-13 20:04:10 +0900

    ⚡️TResGetMainPageHaveToken type 수정
    * issue: [NAILCASE-247](https://nailcase.atlassian.net/browse/NAILCASE-247)
    sub-issues:
    - [NAILCASE-251](https://nailcase.atlassian.net/browse/NAILCASE-251)

--- src/type/mainPage.ts
+++ src/type/mainPage.ts
@@ -1,24 +1,13 @@
-export type TResGetMainPageHaveToken = {
-	recentReservation: null
-	topPopularShops: Array<{
-		shopId: number
-		ownerId: string
-		shopName: string
-		address: string
-		images: null
-	}>
-	likedShops: Array<{
-		shopId: number
-		ownerId: string
-		shopName: string
-		address: string
-		images: null
-	}>
-}
 export type TResShop = {
 	shopId: number
 	ownerId: string
 	shopName: string
 	address: string
 	images: null
+}
+
+export type TResGetMainPageHaveToken = {
+	recentReservation: null
+	topPopularShops: Array<TResShop>
+	likedShops: Array<TResShop>
 }
