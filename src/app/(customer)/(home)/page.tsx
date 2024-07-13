671dd8b39664756763f136eb69501a33f96d022a
keeprok <140785214+keeprok@users.noreply.github.com>
2024-07-13 22:13:30 +0900

    🎨 패칭 Error 시 디자인 추가
    * issue: [NAILCASE-247](https://nailcase.atlassian.net/browse/NAILCASE-247)
    sub-issues:
    - [NAILCASE-251](https://nailcase.atlassian.net/browse/NAILCASE-251)

--- src/app/(customer)/(home)/page.tsx
+++ src/app/(customer)/(home)/page.tsx
@@ -1,22 +1,40 @@
 "use client"
+import { NTButton } from "@/component/common/atom/nt-button"
 import CustomerHeader from "@/component/custom/customer/home/00"
 import CardSlideListForm from "@/component/custom/customer/home/01"
 import UsageForm from "@/component/custom/customer/home/02"
 import ShopListForm from "@/component/custom/customer/home/03"
+import { COMMON_HOME } from "@/constant/routing-path"
 import { useGetMainPageDataQuery } from "@/hook/use-main-controller"
-import type { TResGetMainPageHaveToken } from "@/type/mainPage"
+import type { TResGetMainPageHaveToken } from "@/type/main-page"
 
 import { AroundShop, LikedShop } from "./mockData"
 
 export default function CustomerHome() {
 	const memberId = 2
-	const {
-		data: responseData,
-		isError,
-		error,
-	} = useGetMainPageDataQuery(memberId)
+	const { data: responseData, isError } = useGetMainPageDataQuery(memberId)
 	if (isError) {
-		return <div>Error: {error.message}</div>
+		return (
+			<div className="flex h-dvh w-full flex-col items-center justify-center gap-4">
+				<h1 className="text-Title01 font-SemiBold text-PB100">
+					죄송합니다. 접속 중 오류가 발생했습니다.
+				</h1>
+				<p className="text-Body01 text-Gray70">
+					인터넷 연결 상태를 확인하시고 다시 시도해주세요.
+				</p>
+				<p className="text-Body01 text-Gray70">
+					문제가 지속되면 고객 지원팀에 문의해주세요.
+				</p>
+				<NTButton
+					variant={"primary"}
+					size={"medium"}
+					flexible={"fit"}
+					onClick={() => (window.location.href = COMMON_HOME)}
+				>
+					홈으로
+				</NTButton>
+			</div>
+		)
 	}
 	const $reseponseData = (responseData?.data as TResGetMainPageHaveToken) || {}
 
