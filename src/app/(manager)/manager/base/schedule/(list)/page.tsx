73ddac0f19de4b98501d0acf15a247b14ea525d5
yunshin <2younsin@gmail.com>
2024-07-15 23:47:44 +0900

    ✨ 예약 상태별 예약 목록 조회 api 호출
    * issue: [NAILCASE-322](https://nailcase.atlassian.net/browse/NAILCASE-322)
    sub-issues:
     - [NAILCASE-324](https://nailcase.atlassian.net/browse/NAILCASE-324)

--- src/app/(manager)/manager/base/schedule/(list)/page.tsx
+++ src/app/(manager)/manager/base/schedule/(list)/page.tsx
@@ -1,3 +1,76 @@
-export default function ScheduleList() {
-	return <h1>예약 목록</h1>
+import type { TResGetListReservation } from "@/type"
+import { getListReservation } from "@/util/api/reservation-controller"
+import {
+	decomposeStamp,
+	getBeforeOrAfterN,
+	getNowStamp,
+	getThisDayFirst,
+	getThisDayLast,
+} from "@/util/common"
+
+export default async function ScheduleList() {
+	return <h1>일정 목록</h1>
+}
+
+export const serverFetchReservationList = async (shopId: number) => {
+	let isError = false
+	const pendingList: TResGetListReservation[] = []
+	const confirmedList: TResGetListReservation[] = []
+	const canceledList: TResGetListReservation[] = []
+	const completedList: TResGetListReservation[] = []
+
+	const todayFirst = getTodayFirst()
+	const todayLast = getTodayLast()
+	const before30DaysAgoFromToday = getBeforeOrAfterN(todayLast, 30, "before")
+	const after30DaysAgoFromToday = getBeforeOrAfterN(todayFirst, 30, "after")
+
+	try {
+		const response = await Promise.all([
+			getListReservation(
+				shopId,
+				todayFirst,
+				after30DaysAgoFromToday,
+				"PENDING",
+			),
+			getListReservation(
+				shopId,
+				todayFirst,
+				after30DaysAgoFromToday,
+				"CONFIRMED",
+			),
+			getListReservation(
+				shopId,
+				before30DaysAgoFromToday,
+				todayLast,
+				"CANCELED",
+			),
+			getListReservation(
+				shopId,
+				before30DaysAgoFromToday,
+				todayLast,
+				"COMPLETED",
+			),
+		])
+		pendingList.push(...response[0].dataList)
+		confirmedList.push(...response[1].dataList)
+		canceledList.push(...response[2].dataList)
+		completedList.push(...response[3].dataList)
+	} catch {
+		isError = true
+	}
+	return { isError, pendingList, confirmedList, canceledList, completedList }
+}
+/** 조회일 기준, 00시 00분 반환 */
+const getTodayFirst = () => {
+	const nowStamp = getNowStamp()
+	const { year, month, date } = decomposeStamp(nowStamp)
+	const todayFirstStamp = getThisDayFirst(year, month, date)
+	return todayFirstStamp
+}
+/** 조회일 기준, 23시 59분 59초 반환  */
+const getTodayLast = () => {
+	const nowStamp = getNowStamp()
+	const { year, month, date } = decomposeStamp(nowStamp)
+	const todayLastStamp = getThisDayLast(year, month, date)
+	return todayLastStamp
 }
