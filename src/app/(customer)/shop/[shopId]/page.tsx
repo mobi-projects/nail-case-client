4884c5b74643a7bf4d69d79ce4657c832ada769f
HeesikK <7jjhkhz@naver.com>
2024-07-17 01:07:03 +0900

    🔥 불필요한 코드 삭제
    * issue: [NAILCASE-248](https://nailcase.atlassian.net/browse/NAILCASE-248)
    sub-issues:
    - [NAILCASE-348](https://nailcase.atlassian.net/browse/NAILCASE-348)

--- src/app/(customer)/shop/[shopId]/page.tsx
+++ src/app/(customer)/shop/[shopId]/page.tsx
@@ -1,7 +1,6 @@
 import dynamic from "next/dynamic"
 
 import CustomerShopBanner from "@/component/custom/customer/shop/01"
-import ReservationSchedule from "@/component/custom/customer/shop/02"
 import { convertStringToInteger } from "@/util/common"
 
 const CustomerShopContent = dynamic(
@@ -19,11 +18,9 @@ export default function CustomerShop({ params }: CustomerShopPT) {
 	return (
 		<div className="h-full w-full">
 			<CustomerShopBanner />
-			<div className="mt-[32px] flex w-full flex-col gap-[32px]">
-				<div className="flex w-full flex-col gap-[20px]">
-					<ReservationSchedule shopId={convertStringToInteger(params.shopId)} />
-				</div>
-				<CustomerShopContent />
+			<div className="mt-[32px] flex w-full flex-col">
+				<div className="flex w-full flex-col gap-[20px]"></div>
+				<CustomerShopContent shopId={convertStringToInteger(params.shopId)} />
 			</div>
 		</div>
 	)
--- src/component/custom/customer/shop/05/index.tsx
+++ src/component/custom/customer/shop/05/index.tsx
@@ -1,15 +1,19 @@
 "use client"
 
-import { useRef } from "react"
+import { useRouter } from "next/navigation"
+import type { Dispatch, SetStateAction } from "react"
+import { useRef, useState } from "react"
 
+import { NTButton } from "@/component/common/atom/nt-button"
 import CustomerNaviBar from "@/component/custom/customer/shop/03"
 import ShopInfoCardList from "@/component/custom/customer/shop/04"
 import PostCardList from "@/component/custom/customer/shop/05/01"
 import ShopDesignList from "@/component/custom/customer/shop/05/02"
 import ShopNewsList from "@/component/custom/customer/shop/05/03"
 import ShopReviewList from "@/component/custom/customer/shop/05/04"
+import { getNowStamp } from "@/util/common"
 
-export default function CustomerShopContent() {
+export default function CustomerShopContent({ shopId }: { shopId: number }) {
 	const shopInfoRef = useRef<HTMLDivElement>(null)
 	const designRef = useRef<HTMLDivElement>(null)
 	const newsRef = useRef<HTMLDivElement>(null)
@@ -36,8 +40,12 @@ export default function CustomerShopContent() {
 					if (tool === "리뷰") return handleScroll(reviewRef)
 				}}
 			/>
-			<div ref={shopInfoRef} className="flex w-full flex-col gap-[20px] pt-16">
+			<div
+				ref={shopInfoRef}
+				className="flex w-full flex-col gap-[20px] pt-[32px]"
+			>
 				<ShopInfoCardList />
+				<ReservationBox shopId={shopId} />
 				<p className="text-Title02">네일샵 공지</p>
 				<PostCardList />
 			</div>
@@ -51,5 +59,32 @@ export default function CustomerShopContent() {
 				<ShopReviewList />
 			</div>
 		</div>
+	)
+}
+
+function ReservationBox({ shopId }: { shopId: number }) {
+	const [clickedStamp, setClickedStamp] = useState(getNowStamp())
+
+	return <ReservationButton {...{ clickedStamp, setClickedStamp, shopId }} />
+}
+type ReservationScheduleSubComponentPT = {
+	clickedStamp: number
+	setClickedStamp: Dispatch<SetStateAction<number>>
+	shopId: number
+}
+
+function ReservationButton({ shopId }: ReservationScheduleSubComponentPT) {
+	const router = useRouter()
+	return (
+		<NTButton
+			variant="tertiary"
+			flexible="full"
+			size="small"
+			onClick={() => {
+				router.push(`/shop/${shopId}/reservation`)
+			}}
+		>
+			예약하기
+		</NTButton>
 	)
 }
