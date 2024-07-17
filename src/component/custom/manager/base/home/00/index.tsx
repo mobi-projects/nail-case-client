0aa7761e0efc5e80864e5bca4691b70d8e9c2ff6
keeprok <140785214+keeprok@users.noreply.github.com>
2024-07-17 15:32:19 +0900

    ✨ shop Info데이터 적용
    * issue: [NAILCASE-267](https://nailcase.atlassian.net/browse/NAILCASE-267)

--- src/component/custom/manager/base/home/00/index.tsx
+++ src/component/custom/manager/base/home/00/index.tsx
@@ -11,47 +11,49 @@ export default function ManagerBaseHomeBanner() {
 		return <div>Error: {error.message}</div>
 	}
 	if (isLoading) {
-		return <div>Loading...</div> // 데이터 로딩 중 처리
+		return <div>Loading...</div>
 	}
 	const shopData = data?.data as TResGetShopById
 	return (
 		<div className="relative h-[380px] w-full">
 			<NTBannerImageCarousel className="absolute left-0 h-full w-full bg-transparent" />
-			<BannerHeader />
-			<BannerDescription />
+			<BannerHeader shopData={shopData} />
+			<BannerDescription shopData={shopData} />
 			<NTContent mode="dark" className="absolute right-[78px] top-[62px]">
 				{`${shopData.images?.length.toString() || "0"} /${shopData.images?.length.toString() || 0}`}
 			</NTContent>
 		</div>
 	)
 }
-function BannerHeader() {
+type BannerHeaderPT = { shopData: TResGetShopById }
+function BannerHeader({ shopData }: BannerHeaderPT) {
+	const shopAddress = shopData.address.split(" ").slice(0, 2).join(" ")
 	return (
 		<div className="absolute left-[6%] top-[52.5px]">
 			<p className="text-Callout text-[14px] font-Light text-White">
-				네일아트 전문 | 서울시 용산구
+				네일아트 전문 | {shopAddress}
 			</p>
 			<h1 className="pt-[5.5px] text-Title01 text-[28px] font-Bold text-White">
-				모비네일 한남
+				{shopData.shopName}
 			</h1>
 			<p className="pt-[3px] text-Callout font-SemiBold text-Gray20">
-				오늘 340 · 전체 3,455,666
+				✨ 평점 : {shopData.shopAvgRatings}
 			</p>
 		</div>
 	)
 }
 
-function BannerDescription() {
+function BannerDescription({ shopData }: BannerHeaderPT) {
+	const tagList = shopData.tags.map((tag) => `#${tag}`).join(" ")
 	return (
 		<div className="absolute left-[64px] top-[239px] z-10 flex flex-col gap-4">
 			<div className="flex gap-[13px]">
 				<p className="text-Body01 text-[18px] font-SemiBold text-White">
-					#네일맛집 #주차가능 #오마카세아트
+					{tagList}
 				</p>
 			</div>
 			<p className="line-clamp-3 w-[500px] whitespace-pre-wrap text-Body01 text-[18px] font-Regular text-White">
-				✨ 매달 네일 오마카세를 제공하는 디자인 맛집 모비네일 {`\n`}
-				🔛 현재 당일 예약 가능합니다.
+				{shopData.overview}
 			</p>
 		</div>
 	)
