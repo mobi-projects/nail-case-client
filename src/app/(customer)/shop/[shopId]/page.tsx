380beb77ad3fb430d08bb145d6ec31f9ac97204f
HeesikK <7jjhkhz@naver.com>
2024-07-17 11:28:51 +0900

    ⚡️ 백엔드에 등록되지 않은 매장 접근 제한 추가
    * issue: [NAILCASE-248](https://nailcase.atlassian.net/browse/NAILCASE-248)
    sub-issues:
    - [NAILCASE-311](https://nailcase.atlassian.net/browse/NAILCASE-311)

--- src/app/(customer)/shop/[shopId]/page.tsx
+++ src/app/(customer)/shop/[shopId]/page.tsx
@@ -2,10 +2,10 @@
 
 import dynamic from "next/dynamic"
 import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
 
 import CustomerShopBanner from "@/component/custom/customer/shop/01"
 import ReservationSchedule from "@/component/custom/customer/shop/02"
+import { useShopInfo } from "@/hook/use-shop-controller"
 import { convertStringToInteger } from "@/util/common"
 
 const CustomerShopContent = dynamic(
@@ -19,34 +19,44 @@ type CustomerShopPT = {
 	}
 }
 
-const validShopIds = [1]
-
 export default function CustomerShop({ params }: CustomerShopPT) {
 	const router = useRouter()
-	const [isValidShop, setIsValidShop] = useState(true)
+	const {
+		data: shopInfo,
+		error,
+		isLoading,
+	} = useShopInfo(convertStringToInteger(params.shopId)!)
 
-	useEffect(() => {
-		const shopId = convertStringToInteger(params.shopId)
-		if (!validShopIds.includes(shopId)) {
-			setIsValidShop(false)
-			router.push("/no-results")
-		}
-	}, [params.shopId, router])
-
-	if (!isValidShop) {
-		return null
+	if (isLoading) {
+		return (
+			<div className="flex h-screen items-center justify-center">
+				<div className="flex items-center space-x-2">
+					<div className="h-6 w-6 animate-spin rounded-full border-4 border-solid border-PB100 border-t-transparent"></div>
+					<p className="font-medium text-lg">Loading...</p>
+				</div>
+			</div>
+		)
 	}
 
-	return (
-		<div className="h-full w-full">
-			<CustomerShopBanner />
-			<div className="mt-[32px] flex w-full flex-col gap-[32px]">
-				<div className="flex w-full flex-col gap-[20px]">
-					<p className="text-Title02">예약 일시</p>
-					<ReservationSchedule shopId={convertStringToInteger(params.shopId)} />
+	if (!error && shopInfo && shopInfo.success) {
+		return (
+			<div className="h-full w-full">
+				<CustomerShopBanner />
+				<div className="mt-[32px] flex w-full flex-col gap-[32px]">
+					<div className="flex w-full flex-col gap-[20px]">
+						<p className="text-Title02">예약 일시</p>
+						<ReservationSchedule
+							shopId={convertStringToInteger(params.shopId)}
+						/>
+					</div>
+					<CustomerShopContent />
 				</div>
-				<CustomerShopContent />
 			</div>
-		</div>
-	)
+		)
+	} else {
+		if (typeof window !== "undefined") {
+			router.push("/404")
+		}
+		return null
+	}
 }
