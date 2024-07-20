f7380862a73a8363390a19ab6ab62523806e270d
yunshin <2younsin@gmail.com>
2024-07-20 23:53:55 +0900

    ✨ 예약 요청 거절
    * issue: [NAILCASE-322](https://nailcase.atlassian.net/browse/NAILCASE-322)
    sub-issues:
      - [NAILCASE-346](https://nailcase.atlassian.net/browse/NAILCASE-346)

---
+++ src/component/custom/manager/base/schedule/list/reservation-list/reservation-item/button-pair/decline-modal/decline-modal.type.ts
@@ -0,0 +1,8 @@
+export type DeclineModalPT = {
+	shopId: number
+	reservationId: number
+}
+export type useScheduleCancelMutationFnPT = {
+	shopId: number
+	reservationId: number
+}
--- src/component/custom/manager/base/schedule/list/reservation-list/reservation-item/button-pair/decline-modal/index.tsx
+++ src/component/custom/manager/base/schedule/list/reservation-list/reservation-item/button-pair/decline-modal/index.tsx
@@ -1,3 +1,6 @@
+import { useMutation } from "@tanstack/react-query"
+import { useRouter } from "next/navigation"
+
 import { NTButton } from "@/component/common/atom/nt-button"
 import {
 	ModalBody,
@@ -6,9 +9,23 @@ import {
 	ModalHeader,
 } from "@/component/common/nt-modal"
 import { useModal } from "@/component/common/nt-modal/nt-modal.context"
+import { axiosInstance } from "@/config/axios"
+import getQueryClient from "@/config/tanstack-query/get-query-client"
+import { MANAGER_QUERY } from "@/config/tanstack-query/key-factory"
 
-export default function DeclineModal() {
+import ErrorModal from "../error-modal"
+
+import type {
+	DeclineModalPT,
+	useScheduleCancelMutationFnPT,
+} from "./decline-modal.type"
+
+export default function DeclineModal({
+	shopId,
+	reservationId,
+}: DeclineModalPT) {
 	const { onCloseModal } = useModal()
+	const { mutate: onCancel } = useScheduleCancel()
 	return (
 		<ModalContent>
 			<ModalHeader className="h-[50px] w-full items-center border-b-[1.5px] border-Gray20 text-center text-Body01 font-SemiBold text-Gray90">
@@ -34,10 +51,51 @@ export default function DeclineModal() {
 				>
 					돌아가기
 				</NTButton>
-				<NTButton size="small" flexible="fit" variant="alert">
+				<NTButton
+					size="small"
+					flexible="fit"
+					variant="alert"
+					onClick={() => onCancel({ shopId, reservationId })}
+				>
 					거절하기
 				</NTButton>
 			</ModalFooter>
 		</ModalContent>
 	)
+}
+
+const useScheduleCancel = () => {
+	const queryClient = getQueryClient()
+	const { onOpenModal, onClearModal } = useModal()
+	const router = useRouter()
+	return useMutation({
+		mutationFn: async ({
+			shopId,
+			reservationId,
+		}: useScheduleCancelMutationFnPT) =>
+			postScheduleCancel(shopId, reservationId),
+
+		onSuccess: (_, { shopId }) => {
+			queryClient.invalidateQueries({
+				queryKey: MANAGER_QUERY.scheduleList(shopId).queryKey,
+			})
+			onClearModal()
+		},
+		onError: () => {
+			onClearModal()
+			onOpenModal({
+				children: <ErrorModal bodyText="예약 거절 중, 오류가 발생했습니다." />,
+			})
+		},
+		onSettled: () => {
+			router.refresh()
+		},
+	})
+}
+
+const postScheduleCancel = async (shopId: number, reservationId: number) => {
+	const response = await axiosInstance().patch(
+		`/shops/${shopId}/reservations/${reservationId}/reject`,
+	)
+	return response.data
 }
