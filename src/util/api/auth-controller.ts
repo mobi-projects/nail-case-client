dce2470858210c877c2d0bec156c1b96dd296314
snkchan <144839872+snkchan@users.noreply.github.com>
2024-07-25 15:25:33 +0900

    🐛 로그아웃 api로직 수정
    sub-issues:
     - [NAILCASE-360](https://nailcase.atlassian.net/browse/NAILCASE-360)

--- src/util/api/auth-controller.ts
+++ src/util/api/auth-controller.ts
@@ -1,8 +1,8 @@
 import axios from "axios"
-import { getCookie } from "cookies-next"
+import { getCookie, hasCookie } from "cookies-next"
 
 import { axiosInstance } from "@/config/axios"
-import { REFRESH_TOKEN } from "@/constant/auth-key"
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "@/constant/auth-key"
 import type { TSignType } from "@/type/union-option/sign-type"
 
 /** [GET] 로그인 요청 api 호출 */
@@ -38,13 +38,18 @@ export const postResquestNewToken = async () => {
 
 /** [POST] 로그아웃 요청 api 호출 */
 export const postLogout = async () => {
+	const accessToken = hasCookie(ACCESS_TOKEN)
+		? getCookie(ACCESS_TOKEN)
+		: "no accessToken"
+
 	const refreshToken = getCookie(REFRESH_TOKEN)
 
-	const response = await axiosInstance().post(
-		`/auth/logout`,
+	const response = await axios.post(
+		`${process.env.NEXT_PUBLIC_BACKEND_APP}/auth/logout`,
 		{},
 		{
 			headers: {
+				Authorization: `Bearer ${accessToken}`,
 				"Refresh-Token": `Bearer ${refreshToken}`, // Refresh Token (Header로 보내야 함)
 			},
 		},
