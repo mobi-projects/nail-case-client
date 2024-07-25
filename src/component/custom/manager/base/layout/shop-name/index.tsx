01a25fa41409c144d4f21b90af2bcc9dd111d96f
snkchan <144839872+snkchan@users.noreply.github.com>
2024-07-26 01:04:39 +0900

    ✨ 판매자 header에 data fetching
    sub-issues:
    - [NAILCASE-371](https://nailcase.atlassian.net/browse/NAILCASE-371)

---
+++ src/component/custom/manager/base/layout/shop-name/index.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { useGetUserInfo } from "@/hook/use-auth-controller"
+
+export default function ShopName() {
+	const { data: userInfo } = useGetUserInfo()
+
+	const shopName = userInfo?.data.shopName
+	return (
+		<div className="flex h-[45px] w-[134px] items-center justify-center truncate bg-Gray10 text-Gray100">
+			{shopName}
+		</div>
+	)
+}
