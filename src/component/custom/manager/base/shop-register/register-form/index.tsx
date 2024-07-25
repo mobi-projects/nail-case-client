55792095683e44dfce7eefbb525c61b3d98c46ff
yunshin <2younsin@gmail.com>
2024-07-26 05:07:37 +0900

    ✨ 등록 과정 중, 모든 사용자 입력에 대한 validation
    * issue: [NAILCASE-356](https://nailcase.atlassian.net/browse/NAILCASE-356)
    sub-issues:
     - [NAILCASE-374](https://nailcase.atlassian.net/browse/NAILCASE-374)
    
    desc:
     - 오류 메시지 출력
     - 검증 미통과 시, 버튼 disabled

--- src/component/custom/manager/base/shop-register/register-form/index.tsx
+++ src/component/custom/manager/base/shop-register/register-form/index.tsx
@@ -1,13 +1,49 @@
 "use client"
 
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useMemo, useState } from "react"
+import { useForm } from "react-hook-form"
+
 import { NTButton } from "@/component/common/atom/nt-button"
 
 import ImageForm from "./image-form"
 import InputWrapper from "./input-wrapper"
-import OpeningHoursForm from "./opening-hours-form"
+import { ADDRESS, SHOP_NAME, TELEPHONE } from "./register-form.constant"
+import { SIMPLE_SCHEMA } from "./register-form.schema"
+import { getIsValidOpeningHours, initWorkHours } from "./register-form.util"
+import type { TWorkHour } from "./register.form.type"
 import SimpleInput from "./simple-form"
+import OpeningHoursForm from "./work-hours-form"
 
 export default function ShopRegisterForm() {
+	const {
+		register,
+		getValues,
+		formState: { errors, isValid: isValidSimple },
+	} = useForm({
+		resolver: zodResolver(SIMPLE_SCHEMA),
+		mode: "onBlur",
+		reValidateMode: "onChange",
+	})
+	const [workHours, setWorkHours] = useState<TWorkHour[]>(initWorkHours())
+	const [, setShopProfileFileArr] = useState<Array<File>>([])
+	const [, setPriceListFileArr] = useState<Array<File>>([])
+
+	const errorMsgForShopName = errors[SHOP_NAME.key]?.message as string
+	const errorMsgForAddress = errors[ADDRESS.key]?.message as string
+	const errorMsgForTelephone = errors[TELEPHONE.key]?.message as string
+
+	const isValidOpeningHours = useMemo<boolean>(
+		() => getIsValidOpeningHours(workHours),
+		[workHours],
+	)
+	const isValidToSubmit = isValidSimple && isValidOpeningHours
+
+	const onClickSubmit = () => {
+		const { shopName, address, telephone } = getValues()
+		console.log(shopName, address, telephone)
+	}
+
 	return (
 		<div className="mt-[30px] flex w-full flex-col gap-[65px] px-[20px]">
 			<InputWrapper
@@ -16,7 +52,10 @@ export default function ShopRegisterForm() {
 				description="사업자 등록증에 명시된 상호명을 입력해주세요.(최대 50자)"
 				example="모비네일 한남점"
 			>
-				<SimpleInput />
+				<SimpleInput
+					{...register(SHOP_NAME.key)}
+					errorMessage={errorMsgForShopName}
+				/>
 			</InputWrapper>
 
 			<InputWrapper
@@ -25,7 +64,10 @@ export default function ShopRegisterForm() {
 				description="사업자 등록증에 명시된 영업장 주소을 입력해주세요."
 				example="○○시 ◇◇구 △△동 123-321"
 			>
-				<SimpleInput />
+				<SimpleInput
+					{...register(ADDRESS.key)}
+					errorMessage={errorMsgForAddress}
+				/>
 			</InputWrapper>
 
 			<InputWrapper
@@ -34,15 +76,18 @@ export default function ShopRegisterForm() {
 				description="숫자만 입력해주세요."
 				example="010xxxxxxxx, 02xxxxxxx"
 			>
-				<SimpleInput />
+				<SimpleInput
+					{...register(TELEPHONE.key)}
+					errorMessage={errorMsgForTelephone}
+				/>
 			</InputWrapper>
 
 			<InputWrapper
-				required={true}
+				required={false}
 				label="영업시간"
 				description="요일별, 영업시간을 입력해주세요."
 			>
-				<OpeningHoursForm />
+				<OpeningHoursForm {...{ workHours, setWorkHours }} />
 			</InputWrapper>
 
 			<InputWrapper
@@ -50,7 +95,7 @@ export default function ShopRegisterForm() {
 				label="매장 이미지"
 				description="권장 사이즈: 1280 x 540 (최대 5장)"
 			>
-				<ImageForm maxCount={5} />
+				<ImageForm maxCount={5} setImageFileArr={setShopProfileFileArr} />
 			</InputWrapper>
 
 			<InputWrapper
@@ -58,11 +103,13 @@ export default function ShopRegisterForm() {
 				label="가격표 (이미지)"
 				description="권장 사이즈: 540 x 675 (최대 5장)"
 			>
-				<ImageForm maxCount={5} />
+				<ImageForm maxCount={5} setImageFileArr={setPriceListFileArr} />
 			</InputWrapper>
 
 			<div className="flex h-fit w-full items-center justify-center">
-				<NTButton variant="secondary">등록하기</NTButton>
+				<NTButton disabled={!isValidToSubmit} onClick={onClickSubmit}>
+					등록하기
+				</NTButton>
 			</div>
 		</div>
 	)
--- src/component/custom/manager/base/shop-register/register-form/simple-form/index.tsx
+++ src/component/custom/manager/base/shop-register/register-form/simple-form/index.tsx
@@ -1,14 +1,25 @@
+import type { InputHTMLAttributes } from "react"
 import { forwardRef } from "react"
 
-const SimpleInput = forwardRef<HTMLInputElement>(function SimpleInput(_, ref) {
-	return (
-		<div className="flex h-fit w-full flex-col justify-center gap-[5px]">
-			<input
-				className="h-[35px] w-1/2 min-w-[80px] rounded-[3px] border border-Gray20 px-2 text-Body01 transition-all duration-150 focus:border-2 focus:border-PB110 focus-visible:outline-none"
-				ref={ref}
-			/>
-			<p className="text-Caption02 text-[#FF2C45]">오류남..</p>
-		</div>
-	)
-})
+type SimpleInputPT = InputHTMLAttributes<HTMLInputElement> & {
+	errorMessage?: string
+}
+
+const SimpleInput = forwardRef<HTMLInputElement, SimpleInputPT>(
+	function SimpleInput({ errorMessage, ...rest }, ref) {
+		return (
+			<div className="flex h-[60px] w-full flex-col justify-start gap-[5px]">
+				<input
+					className="h-[35px] w-1/2 min-w-[80px] rounded-[3px] border border-Gray20 px-2 text-Body01 transition-all duration-150 focus:border-2 focus:border-PB110 focus-visible:outline-none"
+					ref={ref}
+					{...rest}
+					autoComplete="off"
+				/>
+				{errorMessage && (
+					<p className="text-Caption02 text-[#FF2C45]">{errorMessage}</p>
+				)}
+			</div>
+		)
+	},
+)
 export default SimpleInput
