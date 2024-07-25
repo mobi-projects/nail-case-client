8d753595e415425379719ecea27366dd149fdf68
snkchan <144839872+snkchan@users.noreply.github.com>
2024-07-25 15:26:14 +0900

    🚚 handleLogout 로직수정 & 관리위치 변경
    sub-issues:
    - [NAILCASE-360](https://nailcase.atlassian.net/browse/NAILCASE-360)

--- src/util/common/auth.ts
+++ src/util/common/auth.ts
@@ -1,7 +1,12 @@
+import { isAxiosError } from "axios"
 import { deleteCookie, setCookie } from "cookies-next"
+import { toast } from "sonner"
 
 import { ACCESS_TOKEN, IS_MANAGER, REFRESH_TOKEN } from "@/constant/auth-key"
+import { COMMON_HOME } from "@/constant/routing-path"
 import type { TSignDataResponse, TRefreshDataResponse } from "@/type"
+
+import { postLogout } from "../api/auth-controller"
 
 export const initAuthTokens = ({
 	accessToken,
@@ -28,4 +33,22 @@ export const deleteAllCookies = () => {
 	deleteCookie(IS_MANAGER)
 	deleteCookie(REFRESH_TOKEN)
 	deleteCookie("profile-image")
+}
+
+export const handleLogout = async () => {
+	try {
+		await postLogout()
+		toast.success("안녕히 가세요")
+	} catch (error) {
+		if (isAxiosError(error) && error.response?.status === 500) {
+			toast.warning("네트워크 문제가 발생했습니다. 다시 로그인 해주세요")
+		} else {
+			toast.error("로그아웃 중 오류가 발생했습니다.")
+		}
+	} finally {
+		deleteAllCookies()
+		setTimeout(() => {
+			window.location.href = COMMON_HOME
+		}, 1000)
+	}
 }
