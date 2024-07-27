e6e254206a75bd3bf83463ffb5b62f1aca5f51f0
keeprok <140785214+keeprok@users.noreply.github.com>
2024-07-28 00:19:31 +0900

    🤡 price image 데이터 설정
    * issue: [NAILCASE-381](https://nailcase.atlassian.net/browse/NAILCASE-381)

---
+++ src/component/custom/manager/base/my-shop/home/03/priceImage.ts
@@ -0,0 +1,6 @@
+export const PriceImage = [
+	"https://picsum.photos/id/189/200/300",
+	"https://picsum.photos/id/193/200/300",
+	"https://picsum.photos/id/201/200/300",
+	"https://picsum.photos/id/222/200/300",
+]
