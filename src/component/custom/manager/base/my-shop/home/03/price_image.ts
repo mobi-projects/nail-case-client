b7fa7a729ebe4ec26baf44e5bdcec2a852d317c7
keeprok <140785214+keeprok@users.noreply.github.com>
2024-07-30 01:33:28 +0900

    🚚 변수 이름 변경
    * sub-issue :
    - [NAILCASE-381](https://nailcase.atlassian.net/browse/NAILCASE-381)

--- src/component/custom/manager/base/my-shop/home/03/price_image.ts
+++ src/component/custom/manager/base/my-shop/home/03/price_image.ts
@@ -1,4 +1,4 @@
-export const PriceImage = [
+export const PRICE_IMAGE = [
 	"https://picsum.photos/id/189/200/300",
 	"https://picsum.photos/id/193/200/300",
 	"https://picsum.photos/id/201/200/300",
