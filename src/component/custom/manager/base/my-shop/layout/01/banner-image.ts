cbfa872d22aba94632f6f69cc1690ac703960ec7
keeprok <140785214+keeprok@users.noreply.github.com>
2024-07-29 23:02:37 +0900

    📚  banner 데이터 추가
    sub-issues:
    - [NAILCASE-366](https://nailcase.atlassian.net/browse/NAILCASE-366)

---
+++ src/component/custom/manager/base/my-shop/layout/01/banner-image.ts
@@ -0,0 +1,6 @@
+export const bannerImage = [
+	{ src: "https://picsum.photos/id/199/1300/800" },
+	{ src: "https://picsum.photos/id/213/1300/800" },
+	{ src: "https://picsum.photos/id/231/1300/800" },
+	{ src: "https://picsum.photos/id/242/1300/800" },
+]
