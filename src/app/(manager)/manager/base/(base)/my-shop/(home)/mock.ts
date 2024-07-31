2f067756bf40c8d557258ec7ab43bfb0688bba82
keeprok <140785214+keeprok@users.noreply.github.com>
2024-07-31 17:01:39 +0900

    🤡 shop-mock-data 추가
    sub-issues:
    - [NAILCASE-393](https://nailcase.atlassian.net/browse/NAILCASE-393)

---
+++ src/app/(manager)/manager/base/(base)/my-shop/(home)/mock.ts
@@ -0,0 +1,62 @@
+export const MOCK_DATA = {
+	workHours: [
+		{
+			closeTime: 1722448800, //오후6시
+			opneTime: 1722420000, //오전 10시
+			isOpen: false,
+			dayOfWeek: 0,
+		},
+		{
+			closeTime: 1722448800, //오후6시
+			opneTime: 1722420000, //오전10시
+			isOpen: true,
+			dayOfWeek: 1,
+		},
+		{
+			closeTime: 1722445200, //오후5시
+			opneTime: 1722416400, // 오전9시
+			isOpen: true,
+			dayOfWeek: 2,
+		},
+		{
+			closeTime: 1722445200, // 오후5시
+			opneTime: 1722416400, // 오전9시
+			isOpen: true,
+			dayOfWeek: 3,
+		},
+		{
+			closeTime: 1722448800, // 오후 6시
+			opneTime: 1722420000, // 오전 10시
+			isOpen: true,
+			dayOfWeek: 4,
+		},
+		{
+			closeTime: 1722448800, // 오후 6시
+			opneTime: 1722416400, // 오전 10시
+			isOpen: true,
+			dayOfWeek: 5,
+		},
+		{
+			closeTime: 1722448800, // 오후 6시
+			opneTime: 1722416400, // 오전 10시
+			isOpen: false,
+			dayOfWeek: 6,
+		},
+	],
+	profileImages: [
+		{ imageId: 0, imageUrl: "https://picsum.photos/id/199/1300/800" },
+		{ imageId: 1, imageUrl: "https://picsum.photos/id/201/1300/800" },
+		{ imageId: 2, imageUrl: "https://picsum.photos/id/202/1300/800" },
+		{ imageId: 3, imageUrl: "https://picsum.photos/id/225/1300/800" },
+		{ imageId: 4, imageUrl: "https://picsum.photos/id/294/1300/800" },
+	],
+	shopName: "모비네일 한남",
+	address: "강남구 역삼동 323 윤신 Tower 1층",
+	priceImages: [
+		{ imageId: 0, imageUrl: "https://picsum.photos/id/199/1300/800" },
+		{ imageId: 1, imageUrl: "https://picsum.photos/id/201/1300/800" },
+		{ imageId: 2, imageUrl: "https://picsum.photos/id/202/1300/800" },
+		{ imageId: 3, imageUrl: "https://picsum.photos/id/205/1300/800" },
+		{ imageId: 4, imageUrl: "https://picsum.photos/id/294/1300/800" },
+	],
+}
