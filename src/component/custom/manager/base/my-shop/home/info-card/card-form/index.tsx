9b0971f1562f45c38d5a24c06844088089b3d660
keeprok <140785214+keeprok@users.noreply.github.com>
2024-08-01 02:39:54 +0900

    ✨ cardForm 클릭 기능 추가
    sub-issues:
    - [NAILCASE-405](https://nailcase.atlassian.net/browse/NAILCASE-405)

--- src/component/custom/manager/base/my-shop/home/info-card/card-form/index.tsx
+++ src/component/custom/manager/base/my-shop/home/info-card/card-form/index.tsx
@@ -2,12 +2,13 @@ import { CardHeader } from "./card-header"
 type CardContainerProps = {
 	title: string
 	children: React.ReactNode
+	onClick?: () => void
 }
 
-export function CardForm({ title, children }: CardContainerProps) {
+export function CardForm({ title, children, onClick }: CardContainerProps) {
 	return (
 		<div className="flex h-[164px] w-[282px] flex-col rounded-[26px] py-[15px] shadow-customGray80">
-			<CardHeader title={title} />
+			<CardHeader title={title} onClick={onClick} />
 			{children}
 		</div>
 	)
