253025d82bdf73be2e42e3cb727a6ee03cd1a9b7
keeprok <140785214+keeprok@users.noreply.github.com>
2024-08-02 15:27:44 +0900

    Merge branch 'main' of https://github.com/mobi-projects/nail-case-client into feat/manager-work-hour

--- src/component/custom/manager/base/my-shop/home/info-card/card-form/card-header/index.tsx
+++ src/component/custom/manager/base/my-shop/home/info-card/card-form/card-header/index.tsx
@@ -2,18 +2,19 @@ import NTIcon from "@/component/common/nt-icon"
 
 type CardHeaderPT = {
 	title: string
+	onClick?: () => void
 }
-export function CardHeader({ title }: CardHeaderPT) {
+export function CardHeader({ title, onClick }: CardHeaderPT) {
 	return (
-		<>
-			<div className="flex w-[282px] items-center justify-between px-[25px] pb-[10px]">
-				<div className="text-Headline02 text-Gray90">{title}</div>
+		<div className="mx-[25px] flex w-[232px] items-center justify-between border-b-[1.5px] border-Gray20 pb-[10px]">
+			<div className="text-Headline02 text-Gray90">{title}</div>
+			{onClick && (
 				<NTIcon
 					icon="expandRightLight"
 					className="h-[24px] w-[24px] cursor-pointer"
+					onClick={onClick}
 				/>
-			</div>
-			<div className="mx-[25px] border-t-[1.5px] border-Gray20"></div>
-		</>
+			)}
+		</div>
 	)
 }
--- src/component/custom/manager/base/my-shop/home/info-card/card-form/index.tsx
+++ src/component/custom/manager/base/my-shop/home/info-card/card-form/index.tsx
@@ -1,13 +1,20 @@
+import type { PropsWithChildren } from "react"
+
 import { CardHeader } from "./card-header"
 type CardContainerProps = {
 	title: string
-	children: React.ReactNode
+
+	onClick?: VoidFunction
 }
 
-export function CardForm({ title, children }: CardContainerProps) {
+export function CardForm({
+	title,
+	children,
+	onClick,
+}: PropsWithChildren<CardContainerProps>) {
 	return (
 		<div className="flex h-[164px] w-[282px] flex-col rounded-[26px] py-[15px] shadow-customGray80">
-			<CardHeader title={title} />
+			<CardHeader title={title} onClick={onClick} />
 			{children}
 		</div>
 	)
