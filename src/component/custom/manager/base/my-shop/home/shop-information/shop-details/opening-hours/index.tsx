af1961be3ae4f757368b8c396eff5dd81bca90f1
keeprok <140785214+keeprok@users.noreply.github.com>
2024-08-06 05:25:01 +0900

    ⚡️ isOpen 분기처리 정리
    sub-issues:
    - [NAILCASE-396](https://nailcase.atlassian.net/browse/NAILCASE-396)

--- src/component/custom/manager/base/my-shop/home/shop-information/shop-details/opening-hours/index.tsx
+++ src/component/custom/manager/base/my-shop/home/shop-information/shop-details/opening-hours/index.tsx
@@ -1,11 +1,8 @@
 import NTIcon from "@/component/common/nt-icon"
 import type { TResGetShopInfo } from "@/util/api-v2/get-shop-info"
 
-import { HourFrom } from "./hour-form"
-import {
-	bulidWorkingTimeFormList,
-	getUnixTimeToOpeningHours,
-} from "./hour.util"
+import { DigitalFormat } from "./digital-format"
+import { bulidWorkingTimeFormList, separateHourMin } from "./hour.util"
 
 type OpeningHourPT = TResGetShopInfo
 
@@ -15,24 +12,28 @@ export default function OpeningHours({ workHours }: OpeningHourPT) {
 			<div className="pb-2 text-Headline02 text-Gray80">영업시간</div>
 			{workHours.map((data, idx) => {
 				const { unixDay, openTime, closeTime } = bulidWorkingTimeFormList(data)
-				const unixOpenTime = getUnixTimeToOpeningHours(openTime)
-				const unixCloseTime = getUnixTimeToOpeningHours(closeTime)
+				const { hour: openHour, minute: openMinute } = separateHourMin(openTime)
+				const { hour: closeHour, minute: closeMinute } =
+					separateHourMin(closeTime)
 				return (
 					<div className="flex w-full items-center gap-x-3 pb-3" key={idx}>
 						<NTIcon icon="dot" className="h-7 w-7" />
 						<p className="text-Body01 text-Gray70">{unixDay}</p>
-						{data.isOpen ? (
-							<div className="flex w-96 justify-between gap-x-1 px-8">
-								<HourFrom title="OPEN" time={unixOpenTime} />
-								<HourFrom title="CLOSE" time={unixCloseTime} />
-							</div>
-						) : (
-							<div className="flex w-96 justify-between gap-x-1 px-8">
-								<HourFrom title="OPEN" time="휴무" />
 
-								<HourFrom title="CLOSE" time="휴무" />
-							</div>
-						)}
+						<div className="flex w-96 justify-between gap-x-1 px-8">
+							<DigitalFormat
+								title="OPEN"
+								hour={openHour.toString()}
+								minute={openMinute.toString().padStart(2, "0")}
+								isOpen={data.isOpen}
+							/>
+							<DigitalFormat
+								title="CLOSE"
+								hour={closeHour.toString()}
+								minute={closeMinute.toString().padStart(2, "0")}
+								isOpen={data.isOpen}
+							/>
+						</div>
 					</div>
 				)
 			})}
