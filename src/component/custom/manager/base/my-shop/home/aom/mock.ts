770b8a7e29886ed47ab7ee2055e462fc8a66b1c3
snkchan <144839872+snkchan@users.noreply.github.com>
2024-08-08 04:55:34 +0900

    🤡 이달의아트 가상정보 추가 & 카메라 아이콘 추가

---
+++ src/component/custom/manager/base/my-shop/home/aom/mock.ts
@@ -0,0 +1,12 @@
+export const aomImageArr = [
+	{ imageUrl: "https://loremflickr.com/320/320/nail", imageId: 1 },
+	{ imageUrl: "https://loremflickr.com/320/320/nailart", imageId: 2 },
+	{ imageUrl: "https://loremflickr.com/320/320/man", imageId: 3 },
+	{ imageUrl: "https://loremflickr.com/320/320/nailart", imageId: 4 },
+	{ imageUrl: "https://loremflickr.com/320/320/food", imageId: 5 },
+	{ imageUrl: "https://loremflickr.com/320/320/fire", imageId: 6 },
+	{ imageUrl: "https://loremflickr.com/320/320/nailart", imageId: 7 },
+	{ imageUrl: "https://loremflickr.com/320/320/nailart", imageId: 8 },
+	{ imageUrl: "https://loremflickr.com/320/320/nailart", imageId: 9 },
+	{ imageUrl: "https://loremflickr.com/320/320/nailart", imageId: 10 },
+]
