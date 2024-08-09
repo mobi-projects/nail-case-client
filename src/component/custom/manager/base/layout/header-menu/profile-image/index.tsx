f65743fb2727c161d202811d137d0175e927076d
snkchan <144839872+snkchan@users.noreply.github.com>
2024-08-09 18:12:04 +0900

    🐛 Image 관련 error 수정

--- src/component/custom/manager/base/layout/header-menu/profile-image/index.tsx
+++ src/component/custom/manager/base/layout/header-menu/profile-image/index.tsx
@@ -3,16 +3,24 @@
 import Image from "next/image"
 
 import { useGetUserInfo } from "@/hook/use-auth-controller"
+import { isUndefined } from "@/util/common/type-guard"
 
 export default function ProfileImage() {
 	const { data: userInfo } = useGetUserInfo()
+	if (isUndefined(userInfo))
+		return (
+			<div className="h-16 w-16 transform animate-pulse rounded-full bg-Gray20" />
+		)
 	return (
-		<Image
-			src={userInfo?.data.profileImage as string}
-			alt="Profile"
-			width={50}
-			height={50}
-			className="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-Gray20"
-		/>
+		<div className="relative flex h-16 w-16 items-center justify-center rounded-full bg-Gray20">
+			<Image
+				src={userInfo.data.profileImage}
+				alt="Profile"
+				fill
+				priority
+				sizes="10vw"
+				className="rounded-full"
+			/>
+		</div>
 	)
 }
