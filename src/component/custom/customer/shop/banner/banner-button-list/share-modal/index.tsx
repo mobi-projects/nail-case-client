b344f735c7968e179db3769bc1a6515aac64c149
keeprok <140785214+keeprok@users.noreply.github.com>
2024-08-09 20:06:06 +0900

    ⚡️ modal 형태 추가 및 nt-modal로 추가
    sub-issues:
    - [NAILCASE-420](https://nailcase.atlassian.net/browse/NAILCASE-420)

--- src/component/custom/customer/shop/banner/banner-button-list/share-modal/index.tsx
+++ src/component/custom/customer/shop/banner/banner-button-list/share-modal/index.tsx
@@ -1,19 +1,11 @@
-import type { Dispatch, SetStateAction } from "react"
-
 import { NTButton } from "@/component/common/atom/nt-button"
-import NTIcon from "@/component/common/nt-icon"
-type ShareAddressModalPT = {
-	isModalOpen: boolean
-	setIsModalOpen: Dispatch<SetStateAction<boolean>>
-}
-export function ShareAddressModal({
-	isModalOpen,
-	setIsModalOpen,
-}: ShareAddressModalPT) {
-	const handleCloseModal = () => {
-		setIsModalOpen(false)
-	}
+import {
+	ModalBody,
+	ModalContent,
+	ModalHeader,
+} from "@/component/common/nt-modal"
 
+export function ShareAddressModal() {
 	const handleCopyUrl = () => {
 		navigator.clipboard.writeText(window.location.href).then(() => {
 			alert("URL이 복사되었습니다.")
@@ -21,29 +13,21 @@ export function ShareAddressModal({
 	}
 
 	return (
-		isModalOpen && (
-			<div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-				<div className="w-[500px] rounded bg-white p-[16px] shadow-lg">
-					<div className="mb-[15px] flex items-center justify-between">
-						<p className="text-Headline02">공유하기</p>
-
-						<NTIcon
-							icon="closeRoundLight"
-							onClick={handleCloseModal}
-							className="cursor-pointer"
-						/>
-					</div>
-					<div className="flex items-center">
-						<input
-							type="text"
-							value={window.location.href}
-							readOnly
-							className="mr-[8px] h-[56px] w-full rounded-[12px] border p-[8px]"
-						/>
-						<NTButton onClick={handleCopyUrl}>복사</NTButton>
-					</div>
+		<ModalContent>
+			<ModalHeader className="mb-4 flex h-16 w-full items-center justify-center border-b-2 px-[15px]">
+				<p className="text-Headline02">공유하기</p>
+			</ModalHeader>
+			<ModalBody className="mb-[15px] flex items-center">
+				<div className="flex w-full items-center">
+					<input
+						type="text"
+						value={window.location.href}
+						readOnly
+						className="mr-[8px] h-[56px] w-full rounded-[12px] border p-[8px]"
+					/>
+					<NTButton onClick={handleCopyUrl}>복사</NTButton>
 				</div>
-			</div>
-		)
+			</ModalBody>
+		</ModalContent>
 	)
 }
