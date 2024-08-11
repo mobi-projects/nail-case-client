e1eec6497d2f1b3102cd8bc7466507b8a5355969
snkchan <144839872+snkchan@users.noreply.github.com>
2024-08-12 00:03:33 +0900

    🎨 클라이언트 패칭에서 발생할 수 있는 loading, error ui 추가
    sub-issues:
    - [NAILCASE-414](https://nailcase.atlassian.net/browse/NAILCASE-414)

---
+++ src/component/custom/manager/base/my-shop/home/aom/aom-error/index.tsx
@@ -0,0 +1,9 @@
+export default function AOMError() {
+	return (
+		<div className="flex h-80 w-full items-center justify-center rounded-3xl border-[3px] border-Gray20">
+			<div className="text-Title01 text-PB100">
+				죄송합니다. 네트워크 문제로 이미지를 불러오지 못했습니다.
+			</div>
+		</div>
+	)
+}
