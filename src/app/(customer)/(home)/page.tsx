f9f99654922709a4e333a2b54c32ae8797a95470
snkchan <144839872+snkchan@users.noreply.github.com>
2024-08-24 03:40:26 +0900

    🎨 reservation-summary 컴포넌트 재구성
    sub-issues:
    - [NAILCASE-431](https://nailcase.atlassian.net/browse/NAILCASE-431)

--- src/app/(customer)/(home)/page.tsx
+++ src/app/(customer)/(home)/page.tsx
@@ -7,16 +7,14 @@ import RecomendShopList from "@/component/custom/customer/home/recomend-shop-lis
 import ReservationSummary from "@/component/custom/customer/home/reservation-summary"
 import { REFRESH_TOKEN } from "@/constant/auth-key"
 
-export default function CustomerHome() {
-	const isLoggedIn = !!getCookie(REFRESH_TOKEN, { cookies }) // CustomerHome은 server 컴포넌트
+export default async function CustomerHome() {
+	const isLoggedIn = !!getCookie(REFRESH_TOKEN, { cookies }) // CustomerHome은 server 컴포넌트이기 때문에 serverCookie로 설정했습니다.
 	return (
 		<div className="flex h-fit flex-col pb-[7px]">
 			<CustomerHeader />
 			<PromotionSection />
 			{isLoggedIn && <ReservationSummary />}
 			<RecomendShopList />
-
-			<div className="flex h-fit w-full flex-col gap-[52px] pb-[33px] pt-[34px]"></div>
 		</div>
 	)
 }
---
+++ src/component/custom/customer/home/reservation-summary/reservation-detail/name-n-image/index.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+
+import type { TMainPageShop } from "@/util/api-v2/get-main-page-data"
+
+type NameNImagePT = {
+	shop: TMainPageShop
+}
+
+export default function NameNImage({ shop }: NameNImagePT) {
+	return (
+		<div className="flex h-full w-fit flex-col items-center justify-center gap-y-3 py-2">
+			<div className="w-48 max-w-48 truncate text-Title03 font-Bold text-PB100">
+				{shop.name}
+			</div>
+			<div className="relative h-48 w-48 rounded-3xl">
+				<Image
+					src={shop.imageSrc}
+					alt="네일샾 이미지"
+					fill
+					priority
+					sizes="500px"
+					className="rounded-3xl"
+				/>
+			</div>
+		</div>
+	)
+}
