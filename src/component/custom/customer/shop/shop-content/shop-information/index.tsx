ba1ed01738c4321897c9ebd3f9f345ec8422bd3f
keeprok <140785214+keeprok@users.noreply.github.com>
2024-09-04 05:28:21 +0900

    🎨  Shop-Aom  기본틀 설정

--- src/component/custom/customer/shop/shop-content/shop-information/index.tsx
+++ src/component/custom/customer/shop/shop-content/shop-information/index.tsx
@@ -1,15 +1,19 @@
+import ShopAom from "./shop-aom"
 import ShopDetails from "./shop-details"
 
-export default function ShopInformation() {
+type ShopInformationPT = { shopId: number }
+export default function ShopInformation({ shopId }: ShopInformationPT) {
+	console.log(shopId)
 	return (
 		<div>
-			<div className="flex min-h-[586px] w-full justify-between px-2 py-5">
-				<div className="flex flex-col gap-[20px]">
-					<p className="text-Title03">내 샵 정보</p>
+			<div className="flex min-h-[586px] w-full justify-between gap-2 px-2 py-5">
+				<div className="flex w-full flex-col items-center gap-9">
+					<p className="text-Title03 font-SemiBold">내 샵 정보</p>
 					<ShopDetails />
 				</div>
-				<div className="flex flex-col gap-[20px]">
-					<p className="text-Title03">이달의 아트</p>
+				<div className="flex w-full flex-col items-center gap-9">
+					<p className="text-Title03 font-SemiBold">이달의 아트</p>
+					<ShopAom shopId={shopId} />
 				</div>
 			</div>
 		</div>
