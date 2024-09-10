7de74529a73dfb94fbd75d355a4cd17c77215e34
keeprok <140785214+keeprok@users.noreply.github.com>
2024-09-11 00:10:07 +0900

    🎨 AOM 대기 화면 수정

--- src/app/(customer)/shop/[shopId]/page.tsx
+++ src/app/(customer)/shop/[shopId]/page.tsx
@@ -1,16 +1,11 @@
 "use client"
 
-import dynamic from "next/dynamic"
 import { useRouter } from "next/navigation"
 
 import CustomerShopBanner from "@/component/custom/customer/shop/banner"
+import CustomerShopContent from "@/component/custom/customer/shop/shop-content"
 import { useShopById } from "@/hook/use-shop-controller"
 import { convertStringToInteger } from "@/util/common"
-
-const CustomerShopContent = dynamic(
-	() => import("@/component/custom/customer/shop/shop-content/index"),
-	{ ssr: false },
-)
 
 type CustomerShopPT = {
 	params: {
@@ -21,12 +16,9 @@ type CustomerShopPT = {
 export default function CustomerShop({ params }: CustomerShopPT) {
 	const router = useRouter()
 	const shopId = params.shopId
-	const {
-		data: shopInfo,
-		error,
-		isLoading,
-	} = useShopById(convertStringToInteger(shopId))
 
+	const { data, error, isLoading } = useShopById(shopId)
+	console.log(data)
 	if (isLoading) {
 		return (
 			<div className="flex h-screen items-center justify-center">
@@ -34,16 +26,21 @@ export default function CustomerShop({ params }: CustomerShopPT) {
 					<div className="h-6 w-6 animate-spin rounded-full border-4 border-solid border-PB100 border-t-transparent"></div>
 					<p className="font-medium text-lg">Loading...</p>
 				</div>
-				<CustomerShopContent shopId={convertStringToInteger(params.shopId)} />
 			</div>
 		)
 	}
 
-	if (!error && shopInfo) {
+	if (!error && data) {
 		return (
 			<div className="h-full w-full">
-				<CustomerShopBanner shopId={convertStringToInteger(params.shopId)} />
-				<CustomerShopContent shopId={convertStringToInteger(params.shopId)} />
+				<CustomerShopBanner
+					shopName={data.shopName}
+					shopAddress={data.address}
+				/>
+				<CustomerShopContent
+					shopId={convertStringToInteger(params.shopId)}
+					data={data}
+				/>
 			</div>
 		)
 	} else {
--- src/component/custom/customer/shop/shop-content/shop-information/shop-aom/aom-skelton/index.tsx
+++ src/component/custom/customer/shop/shop-content/shop-information/shop-aom/aom-skelton/index.tsx
@@ -2,18 +2,16 @@ import AOMImageSingleSkelotn from "./aom-image-single-skeloton"
 
 export default function AOMSkelton() {
 	return (
-		<div className="flex h-80 w-full items-center gap-x-5">
-			<div className="h-80 w-80 min-w-80 transform animate-pulse rounded-3xl bg-Gray20 duration-700" />
-			<div className="flex h-full w-full flex-col justify-evenly gap-y-2 border-l-2 border-l-Gray20 pl-8">
-				<div className="h-6 w-1/4 transform animate-pulse rounded-xl bg-Gray20" />
-				<div className="h-6 w-1/4 transform animate-pulse rounded-xl bg-Gray20" />
-				<div className="flex gap-x-3 pl-8">
-					{Array.from({ length: 5 }, (_, idx) => (
-						<AOMImageSingleSkelotn key={idx} />
-					))}
+		<div className="flex h-full w-[550px] justify-center rounded-[26px] py-12 pl-8 shadow-customGray70">
+			<div className="flex h-[26rem] w-[45rem] items-center justify-center rounded-3xl border-[1.5px] border-black bg-white text-Gray70" />
+			<div className="flex h-full w-full flex-col items-center">
+				<div className="relative flex h-full w-full transform items-center gap-x-5 overflow-x-hidden transition-transform duration-300">
+					<div className="absolute top-1 flex w-full transform flex-col items-center gap-y-1 transition-transform duration-1000">
+						{Array.from({ length: 3 }, (_, idx) => (
+							<AOMImageSingleSkelotn key={idx} />
+						))}
+					</div>
 				</div>
-
-				<div className="h-12 w-20 transform animate-pulse rounded-xl bg-Gray20" />
 			</div>
 		</div>
 	)
