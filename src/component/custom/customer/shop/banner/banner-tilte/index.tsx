537901bc20c99f975848e33ac3aa89142de7bb00
keeprok <140785214+keeprok@users.noreply.github.com>
2024-09-10 23:45:29 +0900

    ⚡️ api 맞춰 props수정

--- src/component/custom/customer/shop/banner/banner-tilte/index.tsx
+++ src/component/custom/customer/shop/banner/banner-tilte/index.tsx
@@ -1,12 +1,13 @@
-import type { TResGetShop } from "@/util/api-v2/get-shop-by-id"
+import type { CustomerShopBannerTitlePT } from ".."
 
-export function BannerTitle({ nailShopInfo }: { nailShopInfo: TResGetShop }) {
-	const { address, shopName } = nailShopInfo
-
+export function BannerTitle({
+	shopAddress,
+	shopName,
+}: CustomerShopBannerTitlePT) {
 	return (
 		<div className="flex flex-col gap-[6px]">
 			<p className="text-Callout font-Light text-White">
-				{`네일아트 전문 | ${address}`}
+				{`네일아트 전문 | ${shopAddress}`}
 			</p>
 			<p className="text-Title01 font-Bold text-White">{shopName}</p>
 		</div>
--- src/component/custom/customer/shop/banner/index.tsx
+++ src/component/custom/customer/shop/banner/index.tsx
@@ -2,39 +2,24 @@
 
 import NTBannerImageCarousel from "@/component/common/nt-banner-image-carousel"
 import NTContent from "@/component/common/nt-content"
-import { useShopById } from "@/hook/use-shop-controller"
-import type { TResGetShop } from "@/util/api-v2/get-shop-by-id"
-import { isUndefined } from "@/util/common/type-guard"
 
 import { BannerButtonList } from "./banner-button-list"
 import { BannerTitle } from "./banner-tilte"
-import { StatusMessage } from "./status-message"
 
-export default function CustomerShopBanner({ shopId }: { shopId: number }) {
-	const {
-		data: shopInfo,
-		isError: isErrorShopInfo,
-		isPending: isPendingShopInfo,
-	} = useShopById(shopId)
-
-	if (isErrorShopInfo || isPendingShopInfo || !shopInfo) {
-		return (
-			<StatusMessage
-				isError={isErrorShopInfo}
-				isPending={isPendingShopInfo}
-				isUndefined={isUndefined(shopInfo)}
-			/>
-		)
-	}
-
-	const nailShopInfo: TResGetShop = shopInfo
-
+export type CustomerShopBannerTitlePT = {
+	shopAddress: string
+	shopName: string
+}
+export default function CustomerShopBanner({
+	shopAddress,
+	shopName,
+}: CustomerShopBannerTitlePT) {
 	return (
 		<div className="flex h-[30rem] w-full flex-col gap-12 px-[75px] pt-20">
 			<NTBannerImageCarousel className="absolute left-0 top-0 z-[-10] h-[30rem] w-full" />
 			<BannerButtonList />
 			<div className="flex h-fit w-full justify-between">
-				<BannerTitle nailShopInfo={nailShopInfo} />
+				<BannerTitle shopAddress={shopAddress} shopName={shopName} />
 				<NTContent mode="dark">0/0</NTContent>
 			</div>
 		</div>
