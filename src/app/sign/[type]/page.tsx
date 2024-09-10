b431ed7396127b70c81a4cff93a9af13ca5f305f
snkchan <144839872+snkchan@users.noreply.github.com>
2024-09-11 01:28:03 +0900

    🐛 main에서 pull

--- src/app/sign/[type]/page.tsx
+++ src/app/sign/[type]/page.tsx
@@ -1,7 +1,7 @@
-import LoginLogowithWelcomeMessage from "@/component/custom/customer/sign/01"
-import SocialLoginButtons from "@/component/custom/customer/sign/02"
-import CheckMessage from "@/component/custom/customer/sign/03/indext"
-import InvalidAccess from "@/component/custom/customer/sign/04"
+import CheckMessage from "@/component/custom/customer/sign/check-message/indext"
+import InvalidAccess from "@/component/custom/customer/sign/invalid-access"
+import LoginWelcomeMessage from "@/component/custom/customer/sign/login-welcome-message"
+import SocialLoginButtons from "@/component/custom/customer/sign/social-login-buttons"
 import type { TSignType } from "@/type/union-option/sign-type"
 type SocialLoginButtonListPT = { params: { type: TSignType } }
 
@@ -15,7 +15,7 @@ export default function SocialLoginButtonList({
 	return (
 		<div className="flex h-dvh flex-col items-center justify-center">
 			<div className="flex h-[70%] w-full flex-col items-center justify-start">
-				<LoginLogowithWelcomeMessage loginType={loginType} />
+				<LoginWelcomeMessage loginType={loginType} />
 				<SocialLoginButtons loginType={loginType} />
 				<CheckMessage loginType={loginType} />
 				<hr className="w-[70%] bg-Gray10" />
