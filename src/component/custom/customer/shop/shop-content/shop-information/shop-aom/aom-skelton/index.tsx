494a8ef06be70251b4a3a45583ba27720faaa13a
keeprok <140785214+keeprok@users.noreply.github.com>
2024-09-12 05:55:45 +0900

    🎨 skelton 디자인 변경

--- src/component/custom/customer/shop/shop-content/shop-information/shop-aom/aom-skelton/index.tsx
+++ src/component/custom/customer/shop/shop-content/shop-information/shop-aom/aom-skelton/index.tsx
@@ -3,7 +3,7 @@ import AOMImageSingleSkelotn from "./aom-image-single-skeloton"
 export default function AOMSkelton() {
 	return (
 		<div className="flex h-full w-[550px] justify-center rounded-[26px] py-12 pl-8 shadow-customGray70">
-			<div className="flex h-[26rem] w-[45rem] items-center justify-center rounded-3xl border-[1.5px] border-black bg-white text-Gray70" />
+			<div className="flex h-[26rem] w-[45rem] items-center justify-center rounded-3xl bg-Gray20 text-Gray70" />
 			<div className="flex h-full w-full flex-col items-center">
 				<div className="relative flex h-full w-full transform items-center gap-x-5 overflow-x-hidden transition-transform duration-300">
 					<div className="absolute top-1 flex w-full transform flex-col items-center gap-y-1 transition-transform duration-1000">
