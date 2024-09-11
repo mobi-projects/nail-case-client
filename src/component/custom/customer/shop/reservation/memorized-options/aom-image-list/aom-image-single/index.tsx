02150113f79de7648f05a845129c469960d7f301
snkchan <144839872+snkchan@users.noreply.github.com>
2024-09-12 02:10:42 +0900

    ✨ 이달의 아트 선택시 보여질ui작업 & 리랜더 최적화

---
+++ src/component/custom/customer/shop/reservation/memorized-options/aom-image-list/aom-image-single/index.tsx
@@ -0,0 +1,75 @@
+import Image from "next/image"
+import type { Dispatch, SetStateAction } from "react"
+
+import { cn } from "@/config/tailwind"
+import type { TAOMImage } from "@/util/api-v2/list-monthly-art"
+
+import type { TReservationForm } from "../.."
+
+type AOMImageSinglePT = {
+	imageInfo: TAOMImage
+	isSelectedAOM: boolean
+	isClicked: boolean
+	setClickedId: Dispatch<SetStateAction<number>>
+	setReservationForm: Dispatch<SetStateAction<TReservationForm>>
+}
+
+export default function AOMImageSingle({
+	imageInfo,
+	isSelectedAOM,
+	isClicked,
+	setClickedId,
+	setReservationForm,
+}: AOMImageSinglePT) {
+	const handleClickImage = () => {
+		setClickedId((prevId) => {
+			if (prevId === imageInfo.imageId) return prevId
+			return imageInfo.imageId
+		})
+		setReservationForm((prev) => ({
+			...prev,
+			treatment: { option: "AOM", imageId: imageInfo.imageId },
+		}))
+	}
+
+	return (
+		<div
+			className={cn(
+				"relative w-24 min-w-24 cursor-pointer rounded-md bg-White shadow-customGray80 transition-all",
+				isSelectedAOM ? "h-24" : "h-1 opacity-0",
+			)}
+			onClick={handleClickImage}
+		>
+			<Image
+				alt="이달의 아트"
+				src={imageInfo.imageUrl}
+				fill
+				sizes="30vw"
+				priority
+				className={cn(
+					"rounded-md",
+					isSelectedAOM ? "border-[3px]" : "border-none",
+					isClicked ? "border-PB80" : "border-transparent",
+				)}
+			/>
+			<ClickedIndicator isSelectedAOM={isSelectedAOM} />
+		</div>
+	)
+}
+
+type ClickedIndicatorPT = {
+	isSelectedAOM: boolean
+}
+
+function ClickedIndicator({ isSelectedAOM }: ClickedIndicatorPT) {
+	return (
+		<div
+			className={cn(
+				"absolute flex w-24 items-center justify-center rounded-md border-2 border-PB50 bg-Gray100/25 font-Bold text-White opacity-0 transition-all",
+				isSelectedAOM ? "h-24 hover:opacity-100" : "h-0 cursor-default",
+			)}
+		>
+			선택
+		</div>
+	)
+}
---
+++ src/component/custom/customer/shop/reservation/memorized-options/index.tsx
@@ -0,0 +1,161 @@
+import { memo, useState, type Dispatch, type SetStateAction } from "react"
+
+import NTOption from "@/component/common/nt-option"
+import type { TNailCondition } from "@/type/union-option/nail-condition"
+import type { TNailTreatment } from "@/type/union-option/nail-treatment"
+import type { TRemoveOption } from "@/type/union-option/remove-option"
+
+import {
+	CONDITION_OPTIONS,
+	EXTENSION_OPTIONS,
+	REMOVE_OPTIONS,
+	TREATMENT_OPTIONS,
+} from "../reservation.constant"
+
+import AOMImageList from "./aom-image-list"
+import SectionTitle from "./section-title"
+
+type TreatmentNConditionPT = {
+	shopId: number
+	reservationForm: TReservationForm
+	setReservationForm: Dispatch<SetStateAction<TReservationForm>>
+}
+
+function TreatmentNCondition({
+	shopId,
+	reservationForm,
+	setReservationForm,
+}: TreatmentNConditionPT) {
+	const [isSelectedAOM, setIsSelectedAOM] = useState(false)
+	const [clickedId, setClickedId] = useState(-1)
+
+	/** 선택된 옵션들의 idx  */
+	const treatmentSelectedIdx = TREATMENT_OPTIONS.valueArr.findIndex(
+		(value) => value === reservationForm.treatment.option,
+	)
+	const removeSelectedIdx = REMOVE_OPTIONS.valueArr.findIndex(
+		(value) => value === reservationForm.remove,
+	)
+	const extendSelectedIdx = EXTENSION_OPTIONS.valueArr.findIndex(
+		(value) => value === reservationForm.extend,
+	)
+	const conditionSelectedIdxArr = reservationForm.conditionList.map(
+		({ option }) =>
+			CONDITION_OPTIONS.valueArr.findIndex((value) => value === option),
+	)
+
+	/** 선택사항 handler함수 */
+	const onClickTreatmentOption = (idx: number) => {
+		const clikedOption = TREATMENT_OPTIONS.valueArr[idx]
+
+		if (reservationForm.treatment.option === clikedOption) return
+		const isAOMSelected = clikedOption === "AOM"
+
+		setIsSelectedAOM(isAOMSelected)
+		setReservationForm((prevForm) => {
+			return {
+				...prevForm,
+				treatment: { option: clikedOption },
+			}
+		})
+	}
+
+	const onClickRemoveOption = (idx: number) => {
+		if (reservationForm.remove === REMOVE_OPTIONS.valueArr[idx]) return
+		setReservationForm((prevForm) => ({
+			...prevForm,
+			remove: REMOVE_OPTIONS.valueArr[idx],
+		}))
+	}
+	const onClickExtendOption = (idx: number) => {
+		if (reservationForm.extend === EXTENSION_OPTIONS.valueArr[idx]) return
+		setReservationForm((prevForm) => ({
+			...prevForm,
+			extend: EXTENSION_OPTIONS.valueArr[idx],
+		}))
+	}
+	const onClickConditionOption = (idx: number) => {
+		setReservationForm((prevForm) => {
+			const { conditionList } = prevForm
+
+			const option = CONDITION_OPTIONS.valueArr[idx]
+			const duplicatedIdx = conditionList.findIndex(
+				({ option: existingOption }) => existingOption === option,
+			)
+			const newConditionList =
+				duplicatedIdx === -1
+					? [...conditionList, { option }]
+					: conditionList.filter((_, i) => i !== duplicatedIdx)
+
+			return {
+				...prevForm,
+				conditionList: newConditionList,
+			}
+		})
+	}
+
+	return (
+		<div className="flex h-fit w-full flex-col gap-[15px] pt-6">
+			<div className="flex flex-col gap-2">
+				<SectionTitle title="시술 내용" isEssential={true} />
+				<NTOption
+					optionArr={TREATMENT_OPTIONS.labelArr}
+					disabledIdxArr={[3]}
+					selectedIdxArr={[treatmentSelectedIdx]}
+					onSelect={onClickTreatmentOption}
+				/>
+			</div>
+			<AOMImageList
+				isSelectedAOM={isSelectedAOM}
+				shopId={shopId}
+				clickedId={clickedId}
+				setClickedId={setClickedId}
+				setReservationForm={setReservationForm}
+			/>
+			<hr className="w-full border-[1px] border-Gray10" />
+			<div className="flex flex-col gap-2 pb-3">
+				<SectionTitle title="네일 제거 유무" isEssential={true} />
+				<NTOption
+					optionArr={REMOVE_OPTIONS.labelArr}
+					selectedIdxArr={[removeSelectedIdx]}
+					onSelect={onClickRemoveOption}
+				/>
+			</div>
+			<hr className="w-full border-[1px] border-Gray10" />
+			<div className="flex flex-col gap-2 pb-3">
+				<SectionTitle title="연장 유무" isEssential={true} />
+				<NTOption
+					optionArr={EXTENSION_OPTIONS.labelArr}
+					selectedIdxArr={[extendSelectedIdx]}
+					onSelect={onClickExtendOption}
+				/>
+			</div>
+			<hr className="w-full border-[1px] border-Gray10" />
+			<div className="flex flex-col gap-2">
+				<SectionTitle title="컨디션" />
+				<NTOption
+					optionArr={CONDITION_OPTIONS.labelArr}
+					selectedIdxArr={conditionSelectedIdxArr}
+					onSelect={onClickConditionOption}
+				/>
+			</div>
+		</div>
+	)
+}
+
+const MemorizedOptions = memo(TreatmentNCondition)
+export default MemorizedOptions
+
+export type TReservationForm = {
+	shopId: number
+	startTime: number
+	remove: TRemoveOption
+	extend: boolean
+	conditionList: Array<{
+		option: TNailCondition
+	}>
+	treatment: {
+		option: TNailTreatment | null
+		imageId?: number
+	}
+}
