939807ca0697a699b8abb1206f045d8d83e70b75
snkchan <144839872+snkchan@users.noreply.github.com>
2024-09-24 23:34:31 +0900

    🐛 경로변경에 따른 toolbar설정 변경

--- src/component/custom/manager/(with-layout)/layout/toolbar/index.tsx
+++ src/component/custom/manager/(with-layout)/layout/toolbar/index.tsx
@@ -4,14 +4,22 @@ import { usePathname, useRouter } from "next/navigation"
 import NTToolbar from "@/component/common/nt-toolbar"
 import { LABEL_LIST_FOR_MANAGER_BASE_TOOLBAR } from "@/constant/toolbar-list"
 
+import type { TStatusExcludeCanceled } from "../../reservations/reservations.type"
+
 import { getFocusedIdx, getToolPathArr } from "./toolbar.util"
 
 export default function ManagerToolbar() {
 	const pathName = usePathname()
-
-	const shopId = pathName.split("/")[2]
+	const pathArr = pathName.split("/")
+	const shopId = pathArr[2]
+	const status = pathArr[4]
+	const page = pathArr[5]
 	const router = useRouter()
-	const toolPathArr = getToolPathArr(shopId)
+	const toolPathArr = getToolPathArr(
+		shopId,
+		page,
+		status as TStatusExcludeCanceled,
+	)
 	const focusedIdx = getFocusedIdx(pathName, toolPathArr)
 
 	const onClickTool = (idx: number) => router.push(toolPathArr[idx])
