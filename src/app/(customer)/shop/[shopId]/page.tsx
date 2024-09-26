b6f09de4f8ac1d0f56ff781ab598200a310902a8
keeprok <140785214+keeprok@users.noreply.github.com>
2024-09-26 22:12:42 +0900

    ⚡️  customer shop 수정

--- src/app/(customer)/shop/[shopId]/page.tsx
+++ src/app/(customer)/shop/[shopId]/page.tsx
@@ -1,11 +1,8 @@
-import {
-	dehydrate,
-	HydrationBoundary,
-	QueryClient,
-} from "@tanstack/react-query"
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query"
 import type { Metadata } from "next"
 
 import { CustomerShopPage } from "@/component/custom/customer/shop/customer-shop"
+import { getCacheClient } from "@/config/tanstack-query"
 import { QUERY_SHOP_INFO } from "@/constant"
 import { getShopById } from "@/util/api-v2/get-shop-by-id"
 
@@ -14,23 +11,23 @@ type CustomerShopPT = {
 		shopId: number
 	}
 }
-export async function generateMetadata({
+export const generateMetadata = async ({
 	params,
-}: CustomerShopPT): Promise<Metadata> {
+}: CustomerShopPT): Promise<Metadata> => {
 	const shopData = await getShopById(params.shopId)
 	const { shopName, address, profileImages } = shopData
 	return {
-		title: `${shopName} - 상세 페이지`,
+		title: `${shopName} - 네일 예약페이지`,
 		description: `매장명은 ${shopName}이고 주소는 ${address}입니다`,
 		openGraph: {
-			title: shopName,
+			title: `${shopName} - 네일 예약페이지`,
 			description: `매장명은 ${shopName}이고 주소는 ${address}입니다`,
 			images: profileImages[0].imageUrl,
 		},
 	}
 }
 export default async function CustomerShop({ params }: CustomerShopPT) {
-	const queryClient = new QueryClient()
+	const queryClient = getCacheClient()
 
 	await queryClient.prefetchQuery({
 		queryKey: [QUERY_SHOP_INFO, params.shopId],
