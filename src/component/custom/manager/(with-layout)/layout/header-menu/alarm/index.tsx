795c7451e6bd4ac69f269b560450b1aa44c46aa4
snkchan <144839872+snkchan@users.noreply.github.com>
2024-10-10 00:03:38 +0900

    🎨 alarm 아이콘에 pulldown, sse연결

--- src/component/custom/manager/(with-layout)/layout/header-menu/alarm/index.tsx
+++ src/component/custom/manager/(with-layout)/layout/header-menu/alarm/index.tsx
@@ -1,5 +1,50 @@
+"use client"
+
+import { getCookie } from "cookies-next"
+import dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime"
+import { useState } from "react"
+
 import NTIcon from "@/component/common/nt-icon"
+import { useNTPulldown } from "@/component/common/nt-pulldown"
+import { cn } from "@/config/tailwind"
+import { ACCESS_TOKEN } from "@/constant/auth-key"
+import { useSSE, type TResSubscribe } from "@/hook/use-sse"
+
+import AlarmPulldown from "./alarm-pulldown"
+
+dayjs.extend(relativeTime)
 
 export default function Alarm() {
-	return <NTIcon className="text-Gray90" icon="bellLight" />
+	const [message, setMessage] = useState<Array<TResSubscribe>>([])
+	const [connect, setConnect] = useState(false) // SSE연결을 제어하기위한 임시상태 추후 제거예정
+	const { setIsOpen } = useNTPulldown()
+	const accessToken = getCookie(ACCESS_TOKEN)
+	const hasNewReservation = message.length > 0
+	useSSE(setMessage, connect, accessToken)
+
+	return (
+		<div className="relative flex h-fit w-fit">
+			<button
+				onClick={() => {
+					setConnect((prev) => !prev)
+					if (connect) {
+						setMessage([])
+					}
+				}}
+			>
+				연결
+			</button>
+			<NTIcon
+				className={cn("text-Gray90", hasNewReservation && "cursor-pointer")}
+				icon="bellLight"
+				onClick={() => {
+					if (hasNewReservation) {
+						setIsOpen((prev) => !prev)
+					}
+				}}
+			/>
+			{hasNewReservation && <AlarmPulldown message={message} />}
+		</div>
+	)
 }
--- src/component/custom/manager/(with-layout)/layout/header-menu/index.tsx
+++ src/component/custom/manager/(with-layout)/layout/header-menu/index.tsx
@@ -1,3 +1,5 @@
+import { NTPulldownProvider } from "@/component/common/nt-pulldown"
+
 import Alarm from "./alarm"
 import LogoutButton from "./logout-button"
 import ProfileImage from "./profile-image"
@@ -5,7 +7,9 @@ import ProfileImage from "./profile-image"
 export default function HeaderMenu() {
 	return (
 		<div className="flex w-[236px] items-center justify-end gap-[12px] pr-[21px]">
-			<Alarm />
+			<NTPulldownProvider>
+				<Alarm />
+			</NTPulldownProvider>
 			<ProfileImage />
 			<LogoutButton />
 		</div>
