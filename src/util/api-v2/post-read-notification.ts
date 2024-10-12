25a7e3b19cb48dacbb56c6be2377e467d9225367
snkchan <144839872+snkchan@users.noreply.github.com>
2024-10-13 02:45:16 +0900

    ✨ 새로운 알림 조회 api & hook작성

---
+++ src/util/api-v2/post-read-notification.ts
@@ -0,0 +1,19 @@
+import { useMutation } from "@tanstack/react-query"
+
+import { axiosInstance } from "@/config/axios"
+
+export const postReadNotification = async (reqBody: TReqReadNotification) => {
+	const response = await axiosInstance().post(`/notification/read`, reqBody)
+	console.log(response)
+	return response
+}
+
+export type TReqReadNotification = {
+	notificationId: number
+}
+
+export const useMutateReadNotification = () =>
+	useMutation({
+		mutationFn: (reqBody: TReqReadNotification) =>
+			postReadNotification(reqBody),
+	})
