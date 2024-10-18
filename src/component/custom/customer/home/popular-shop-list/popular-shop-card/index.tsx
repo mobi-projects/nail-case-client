bfc39a7616e920f2864c321032ddbf434eaa38a7
snkchan <144839872+snkchan@users.noreply.github.com>
2024-10-19 04:02:36 +0900

    🐛 인기샵 list 디자인 수정 & routing bug수정

--- src/component/custom/customer/home/popular-shop-list/popular-shop-card/index.tsx
+++ src/component/custom/customer/home/popular-shop-list/popular-shop-card/index.tsx
@@ -16,9 +16,18 @@ type PopularShopCardPT = {
 }
 
 export function PopularShopCard({ shop }: PopularShopCardPT) {
+	const isLoggedIn = !!getCookie(REFRESH_TOKEN)
+	const router = useRouter()
+	const handleRouting = () => {
+		if (isLoggedIn) {
+			router.push(`shop/${shop.shopId}`)
+		} else {
+			router.push(`${COMMON_SIGN}/member`)
+		}
+	}
 	return (
 		<div className="flex h-fit w-fit flex-col gap-[13px]">
-			<ShopName shop={shop} />
+			<ShopName shop={shop} handleRouting={handleRouting} />
 			<div className="group relative z-10 h-[264px] w-[384px] cursor-pointer rounded-[26px] bg-Gray40">
 				<Image
 					src={shop.shopImageUrl || PromotionImage}
@@ -34,25 +43,22 @@ export function PopularShopCard({ shop }: PopularShopCardPT) {
 						shop.likedByUser ? "text-PY80" : "text-White/80",
 					)}
 				/>
-				<ShopHoverInfo shop={shop} />
+				<ShopHoverInfo shop={shop} handleRouting={handleRouting} />
 			</div>
 		</div>
 	)
 }
 
-function ShopHoverInfo({ shop }: PopularShopCardPT) {
-	const isLoggedIn = !!getCookie(REFRESH_TOKEN)
-	const router = useRouter()
+type ShopHoverInfoPT = {
+	shop: TPopularShop
+	handleRouting: VoidFunction
+}
+
+function ShopHoverInfo({ shop, handleRouting }: ShopHoverInfoPT) {
 	return (
 		<div
-			onClick={() => {
-				if (isLoggedIn) {
-					router.push(`shop/${shop.shopId}`)
-				} else {
-					router.push(`${COMMON_SIGN}/member`)
-				}
-			}}
-			className="absolute inset-0 flex flex-col justify-between rounded-[26px] bg-gradient-to-tr from-Black/50 to-Gray30 py-[19px] pl-[29px] text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"
+			onClick={handleRouting}
+			className="absolute inset-0 flex flex-col justify-between rounded-[26px] bg-gradient-to-tr from-Black/60 to-Black/10 py-[19px] pl-[29px] text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"
 		>
 			<div className="pt-[10]">
 				<div className="text-Headline01 text-PY100">{shop.shopName}</div>
--- src/component/custom/customer/home/popular-shop-list/popular-shop-card/shop-name/index.tsx
+++ src/component/custom/customer/home/popular-shop-list/popular-shop-card/shop-name/index.tsx
@@ -1,18 +1,14 @@
-import { useRouter } from "next/navigation"
-
 import type { TPopularShop } from "@/util/api/get-top-popular-shops"
 
 type ShopNamePT = {
 	shop: TPopularShop
+	handleRouting: VoidFunction
 }
-export function ShopName({ shop }: ShopNamePT) {
-	const router = useRouter()
+export function ShopName({ shop, handleRouting }: ShopNamePT) {
 	return (
 		<div
 			className="h-fit w-fit cursor-pointer pl-4 text-Body01 font-SemiBold text-Gray80"
-			onClick={() => {
-				router.push(`shop/${shop.shopId}`)
-			}}
+			onClick={handleRouting}
 		>
 			{shop.shopName}
 		</div>
