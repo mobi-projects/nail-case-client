c607d9ee7d45d9a4eb189c908acc2efc13181639
snkchan <144839872+snkchan@users.noreply.github.com>
2024-10-23 01:05:50 +0900

    🎨 가게 상세 반응형 수정

--- src/component/custom/customer/shop/customer-shop/shop-content/shop-information/shop-aom/aom-skelton/index.tsx
+++ src/component/custom/customer/shop/customer-shop/shop-content/shop-information/shop-aom/aom-skelton/index.tsx
@@ -1,12 +1,12 @@
 export default function AOMSkelton() {
 	return (
-		<div className="relative flex h-[30rem] w-full px-2">
+		<div className="relative flex h-[30rem] w-full px-2 max-xl:px-6">
 			<div className="w-full gap-4">
-				<div className="flex gap-4">
+				<div className="flex gap-8">
 					{[...Array(3)].map((_, idx) => (
 						<div
 							key={idx}
-							className="animation-pulse h-[26rem] w-[32.33%] flex-shrink-0 rounded-3xl bg-Gray20 px-2 text-Gray70 shadow-customGray80 md:h-[12rem] md:w-[12rem] lg:h-[16rem] lg:w-[16rem] xl:h-[18rem] xl:w-[18rem] max-sm:h-[8rem] max-sm:w-[8rem]"
+							className="animation-pulse h-[26rem] w-[32.33%] flex-shrink-0 rounded-3xl bg-Gray20 px-2 text-Gray70 shadow-customGray80 md:h-[10rem] md:w-[13rem] lg:h-[16rem] lg:w-[29dvw] xl:h-[20rem] xl:w-[29dvw] max-sm:h-[10rem] max-sm:w-[10rem] max-md:px-5"
 						/>
 					))}
 				</div>
