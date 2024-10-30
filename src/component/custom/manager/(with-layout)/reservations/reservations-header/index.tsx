d541fcbbf2012013fe9b42d72dac34ad8d41ba6f
snkchan <144839872+snkchan@users.noreply.github.com>
2024-10-30 17:02:03 +0900

    🎨 예약관리 페이지 반응형 사이드바 추가

--- src/component/custom/manager/(with-layout)/reservations/reservations-header/index.tsx
+++ src/component/custom/manager/(with-layout)/reservations/reservations-header/index.tsx
@@ -2,6 +2,7 @@ import { STATUS_WITHOUT_CANCELED_ARR } from "../reservations.constant"
 import type { TStatusExcludeCanceled } from "../reservations.type"
 
 import CategoryBox from "./catagory-box"
+import MobileReservationSideBar from "./mobile-reservation-side-bar"
 
 type ReservationsHeaderPT = {
 	status: TStatusExcludeCanceled
@@ -11,14 +12,17 @@ export default function ReservationsHeader({
 	status: curStatus,
 }: ReservationsHeaderPT) {
 	return (
-		<div className="mt-10 flex w-full items-center justify-between pb-8">
-			{STATUS_WITHOUT_CANCELED_ARR.map((status, idx) => (
-				<CategoryBox
-					status={status}
-					key={status}
-					isClicked={STATUS_WITHOUT_CANCELED_ARR[idx] === curStatus}
-				/>
-			))}
+		<div className="h-full w-full">
+			<div className="mt-10 flex w-full items-center justify-between pb-8 max-lg:hidden">
+				{STATUS_WITHOUT_CANCELED_ARR.map((status, idx) => (
+					<CategoryBox
+						status={status}
+						key={status}
+						isClicked={STATUS_WITHOUT_CANCELED_ARR[idx] === curStatus}
+					/>
+				))}
+			</div>
+			<MobileReservationSideBar />
 		</div>
 	)
 }
