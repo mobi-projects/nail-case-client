33b96cc2208e3608d34e80333a13c38a6c00ca27
snkchan <144839872+snkchan@users.noreply.github.com>
2024-11-02 00:39:50 +0900

    🐛 프로필 image skelton수정

--- src/component/custom/manager/(with-layout)/layout/header-menu/profile-image/index.tsx
+++ src/component/custom/manager/(with-layout)/layout/header-menu/profile-image/index.tsx
@@ -9,7 +9,7 @@ export default function ProfileImage() {
 	const { data: userInfo, isLoading } = useGetUserInfo("MANAGER")
 	if (isUndefined(userInfo) || isLoading)
 		return (
-			<div className="h-16 w-16 transform animate-pulse rounded-full bg-Gray20" />
+			<div className="h-16 w-16 transform animate-pulse rounded-full bg-Gray20 lg:h-12 lg:w-12 max-md:h-10 max-md:w-10" />
 		)
 	return (
 		<div className="relative flex h-16 w-16 items-center justify-center rounded-full bg-Gray20 lg:h-12 lg:w-12 max-md:h-10 max-md:w-10">
