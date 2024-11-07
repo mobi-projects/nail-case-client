9ddd19daf1797bd2e303037d926f6c6bb6edc1d6
snkchan <144839872+snkchan@users.noreply.github.com>
2024-11-07 17:30:59 +0900

    🎨 로딩스피너 사이즈 변경

--- src/app/(customer)/shop/[shopId]/loading.tsx
+++ src/app/(customer)/shop/[shopId]/loading.tsx
@@ -4,7 +4,7 @@ export default function ShopDetailLoading() {
 	return (
 		<div className="flex h-screen items-center justify-center">
 			<div className="flex flex-col">
-				<NTLoadingSpinner size="large" />
+				<NTLoadingSpinner size="medium" />
 				<p className="font-medium text-lg">Loading...</p>
 			</div>
 		</div>
