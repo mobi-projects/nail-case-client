958ae458a14dfbf980573ee623911719c7ca2b58
snkchan <144839872+snkchan@users.noreply.github.com>
2025-04-09 21:35:53 +0900

    관리자 홈 layoutshift 수정

--- src/component/custom/manager/(with-layout)/(home)/shop-information/index.tsx
+++ src/component/custom/manager/(with-layout)/(home)/shop-information/index.tsx
@@ -8,13 +8,13 @@ export default function ShopInformation({ shopId }: ShopInformation) {
 	return (
 		<div className="max-lg:px-3">
 			<div className="flex min-h-[586px] w-full justify-between px-2 py-5 lg:justify-evenly lg:gap-x-6 max-md:flex-col max-md:gap-y-6">
-				<div className="flex flex-col gap-[20px]">
-					<p className="min-h-[400px] text-Title03 md:text-[16px] max-sm:text-[14px]">
+				<div className="flex min-h-[457px] flex-col gap-[20px]">
+					<p className="text-Title03 md:text-[16px] max-sm:text-[14px]">
 						내 샵 정보
 					</p>
 					<ShopDetails shopId={shopId} />
 				</div>
-				<div className="flex min-h-[400px] flex-col gap-[20px]">
+				<div className="flex min-h-[442px] flex-col gap-[20px]">
 					<p className="text-Title03 md:text-[16px] max-sm:text-[14px]">
 						필수 예약 사항
 					</p>
